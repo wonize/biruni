982e6d861bc1e7dacd83997737ef103753179b0b
mikoloism <mikoloism@gmail.com>
2024-04-15 18:00:49 +0330

    test(plugin_localstorage): improve spec/test cases
    improve cases to be real-world example as simple

--- tests/plugin_localstorage.spec.ts
+++ tests/plugin_localstorage.spec.ts
@@ -1,7 +1,10 @@
-import { LocalStoragePlugin, NativeJsonPlugin, biruni } from "@/core/mod";
+import { LocalStoragePersister, LocalStoragePlugin, NativeJsonPlugin, biruni, type Core } from "@/core/mod";
+import { beforeAll, describe, it, vi } from 'vitest';
 
-describe('Biruni LocalStorage', () => {
-	let store: any;
+describe('Biruni :: simple setup', () => {
+	const setSpy = vi.spyOn(LocalStoragePersister.prototype, 'set');
+
+	let store: Core<{ count: number }>;
 	beforeAll(() => {
 		store = biruni()
 			.plug(NativeJsonPlugin())
@@ -9,7 +12,35 @@ describe('Biruni LocalStorage', () => {
 			.init(() => ({ count: 9 }));
 	})
 
-	it('should work', () => {
-		expect(store.get('b-key')).resolves.toMatchObject({ count: 9 });
+	it('should iniailize by correct value', () => {
+		const expection = { "$$value": JSON.stringify({ count: 9 }) };
+		expect(setSpy).toBeCalledWith(expection);
+	})
+
+	it('should <localStorage.set> called once', () => {
+		expect(setSpy).toBeCalledTimes(1);
+	})
+
+	it('should restore initialized value', () => {
+		expect(store.get()).resolves.toMatchObject({ count: 9 });
+	})
+
+	it('should override initialized value', () => {
+		const expection = { "$$value": JSON.stringify({ count: 7 }) };
+		store.set(() => ({ count: 7 }));
+		expect(setSpy).toBeCalledWith(expection);
+	})
+
+	it('should <localStorage.set> called twice', () => {
+		expect(setSpy).toBeCalledTimes(2);
+	})
+
+	it('should restore overrided value', () => {
+		expect(store.get()).resolves.toMatchObject({ count: 7 });
+	})
+
+	afterAll(() => {
+		setSpy.mockClear();
+		setSpy.mockReset();
 	})
 })
