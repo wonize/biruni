add4d4e6625db0acc93ad2c4af05f60d56f96a9c
mikoloism <mikoloism@gmail.com>
2024-04-17 17:44:00 +0330

    test(setup): add setupFiles as file `tests/`

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [
 		tsconfigPaths(),
 	],
 	test: {
+		name: 'unit',
 		globals: true,
 		environment: 'jsdom',
 		mockReset: false,
-		setupFiles: ['vitest-localstorage-mock'],
+		setupFiles: ['./tests/setup.ts'],
 	},
 });
