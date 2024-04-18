152efecf4e89e1e6452ee9be992aefc8c5727d30
mikoloism <mikoloism@gmail.com>
2024-04-18 14:41:08 +0330

    feat(react): symbolic add `useSubscribe` hook function

---
+++ packages/react/src/useSubscribe.ts
@@ -0,0 +1,5 @@
+import type { Core } from 'biruni';
+
+export function useSubscribe<TValue extends object>(store: Core<TValue>) {
+	throw new Error('[BIRUNI] <useSubscribe> not implemented and in WIP')
+}
