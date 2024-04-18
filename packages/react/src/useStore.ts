99e6e7a28d91ed9e75083247ef2863eb558fa3bb
mikoloism <mikoloism@gmail.com>
2024-04-18 14:39:20 +0330

    feat(react): add `useStore` hook
    implement `useStore` hook based on `core`

---
+++ packages/react/src/useStore.ts
@@ -0,0 +1,36 @@
+import type { Core } from 'biruni';
+import React from 'react';
+
+function useStore<V extends object>(store: Core<V>) {
+	const [value, setValue] = React.useState<V>();
+
+	React.useEffect(() => {
+		store.get().then((value) => {
+			setValue(() => value)
+		})
+	}, [store.get]);
+
+	const $$store = React.useMemo(() => ({
+		set: function (setter: V | ((d?: V) => V)) {
+			store.set(setter as any)
+		},
+		get: function <
+			K extends keyof V,
+			P extends undefined | K = undefined,
+			R = P extends K ? V[P] : V,
+		>(property?: P): R {
+			if (typeof value === 'undefined') return {} as unknown as never;
+
+			if (typeof property === 'undefined') {
+				return value as unknown as never;
+			} else {
+				return value[property] as unknown as never;
+			}
+		}
+	}), [value]);
+
+	return $$store;
+}
+
+export { useStore as useBiruni, useStore };
+
