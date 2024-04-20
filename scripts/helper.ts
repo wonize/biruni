df698f2065874cc3f99f8162c3dd9c980421bda1
mikoloism <mikoloism@gmail.com>
2024-04-20 16:57:15 +0330

    ci(scripts): refactor usage of helper
    extract `fileURLToPath(new URL(path, base))` to ownself function named `r(path, base=cwd)`

--- scripts/helper.ts
+++ scripts/helper.ts
@@ -1,41 +1,57 @@
 import 'zx/globals';
 import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { fileURLToPath, format } from 'node:url';
 import { default as process } from 'node:process';
 import type { Options } from 'tsup';
 import { default as mergePackageJson } from './packagejson';
 
 $.verbose = false;
 
+const r = (path: string, base?: string) => {
+	return fileURLToPath(
+		format({
+			protocol: 'file',
+			hostname: base ?? process.cwd(),
+			pathname: path,
+		}),
+	);
+};
+
 type Config = Partial<Omit<Options, 'entryPoints' | 'outDir' | 'clean'>>;
 
-/** @param pkg {string} 'packages/core' */
-function b(pkg: string, config?: Config): Options {
-	const cwd = dirname($.cwd ?? process.cwd());
-	const path = fileURLToPath(new URL(pkg, cwd));
-	const entry = fileURLToPath(new URL('./src/mod.ts', path));
-	const outDir = fileURLToPath(new URL('./dist', path));
+/** @param workspace {string} 'packages/core' */
+function b(workspace: string, config?: Config): Options {
+	const cwd = process.cwd();
+	const base = r(workspace, cwd);
+	const entry = r('src/mod.ts', base);
+	const outDir = r('dist', base);
+	const tsconfig = r('tsconfig.json', base);
 
 	const onSuccess = async () => {
-		const path_packagejson = fileURLToPath(new URL('./package.json', path));
-		await $`cp ${path_packagejson} ${outDir.concat('/')}`
+		const path_packagejson = r('package.json', base);
+		await $`cp ${path_packagejson} ${outDir.concat('/')}`;
 
-		const path_license = fileURLToPath(new URL('./LICENSE', path));
-		await $`cp ${path_license} ${outDir.concat('/')}`
+		const path_license = r('LICENSE', base);
+		await $`cp ${path_license} ${outDir.concat('/')}`;
 
-		const path_readme = fileURLToPath(new URL('./README.md', path));
-		await $`cp ${path_readme} ${outDir.concat('/')}`
+		const path_readme = r('README.md', base);
+		await $`cp ${path_readme} ${outDir.concat('/')}`;
 
-		const raw_packagejson = await $`cat ${path_packagejson}`
+		const raw_packagejson = await $`cat ${path_packagejson}`;
 		const packagejson = JSON.parse(raw_packagejson.stdout);
-		const merged_packagejson = mergePackageJson(pkg, packagejson);
-		const out_packagejson = fileURLToPath(new URL('./package.json', outDir));
-		await fs.promises.writeFile(out_packagejson, JSON.stringify(merged_packagejson, null, 2), 'utf-8');
+		const merged_packagejson = mergePackageJson(workspace, packagejson);
+		const out_packagejson = r('./package.json', outDir);
+		await fs.promises.writeFile(
+			out_packagejson,
+			JSON.stringify(merged_packagejson, null, 2),
+			'utf-8',
+		);
 
-		echo(`  MOVE :: ${path}`)
-	}
+		echo(`  PREPARE :: ${base}`);
+	};
 
 	const option: Options = {
+		name: workspace,
 		dts: true,
 		shims: true,
 		clean: true,
@@ -48,20 +64,19 @@ function b(pkg: string, config?: Config): Options {
 		outDir: outDir,
 		entry: [entry],
 		format: ['cjs', 'esm'],
+		tsconfig: tsconfig,
 		outExtension: (o) => ({
-			'js': o.format === 'esm'
-				? '.mjs'
-				: '.cjs'
+			js: o.format === 'esm' ? '.mjs' : '.cjs',
 		}),
-	}
+	};
 
-	const pkgSuccess = config?.onSuccess as (() => Promise<void>);
+	const pkgSuccess = (config?.onSuccess ?? (async () => { })) as () => Promise<void>;
 	delete config?.onSuccess;
 	return Object.assign({}, option, config, {
 		onSuccess: async () => {
 			await onSuccess();
 			await pkgSuccess();
-		}
+		},
 	});
 }
 
