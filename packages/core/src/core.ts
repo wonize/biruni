39d78bfa5084c8ac9e096ce5175544ee48f229de
mikoloism <mikoloism@gmail.com>
2024-04-20 18:49:40 +0330

    feat(core): symbolic support `key and setter` feature
    related to: 560e5c3d31ee5b1bd4b52fa75aacb30ecb5d26f2
    
    it's just work with types annotations and not implemented

--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -1,35 +1,47 @@
 import type { ParserContext } from './parser/context';
 import type { PersisterContext } from './persister/context';
 import type { PluginStruct } from './plugin/struct';
-import type { SetterArg, ValueArg } from './proxy';
+import type { SetFn, SetFnByKey, SetObject, SetObjectByKey } from './generic';
 
-export class Core<V extends object> {
+export class Core<S extends object> {
 	public constructor(
-		private initializer: () => V,
-		private pluginStruct: PluginStruct<V>,
+		private initializer: () => S,
+		private pluginStruct: PluginStruct<S>,
 	) {
 		this.setByValue(this.initializer());
 	}
 
-	async set(value: ValueArg<V>): Promise<void>;
-	async set(setter: SetterArg<V>): Promise<void>;
-	async set(valueOrSetter: ValueArg<V> | SetterArg<V>): Promise<void> {
-		if (typeof valueOrSetter === 'function') {
-			return this.setBySetter(valueOrSetter);
+	async set<K = undefined>(o: SetObject<S>): Promise<void>;
+	async set<K = undefined>(fn: SetFn<S>): Promise<void>;
+	async set<K extends keyof S, V = SetObjectByKey<S, K>>(key: K, value: V): Promise<void>;
+	async set<K extends keyof S, Fn = SetFnByKey<S, K>>(key: K, fn: Fn): Promise<void>;
+	async set<
+		K extends keyof S,
+		P extends SetObject<S> | SetFn<S> | K,
+		G = P extends K ? (S[P] | ((draft?: S[P]) => S[P])) : (P extends SetFn<S> ? never : never)
+	>(KeyOrFnOrObj: P, FnOrObj?: G): Promise<void> {
+		if (typeof KeyOrFnOrObj === 'function') {
+			return this.setBySetter(KeyOrFnOrObj as SetFn<S>);
+		} else if (typeof KeyOrFnOrObj === 'string') {
+			if (typeof FnOrObj === 'function') {
+				return this.setByKeySetter(KeyOrFnOrObj as K, FnOrObj as SetFnByKey<S, K>);
+			} else {
+				return this.setByKeyValue(KeyOrFnOrObj as K, FnOrObj as SetObjectByKey<S, K>);
+			}
 		} else {
-			return this.setByValue(valueOrSetter);
+			return this.setByValue(KeyOrFnOrObj as SetObject<S>);
 		}
 	}
 
-	private async setBySetter(setter: SetterArg<V>): Promise<void> {
+	private async setBySetter(fn: SetFn<S>): Promise<void> {
 		return new Promise(async (resolve) => {
-			const parser = this.pluginStruct?.parser as ParserContext<V>;
+			const parser = this.pluginStruct?.parser as ParserContext<S>;
 			if (typeof parser === 'undefined') throw 'should have least one parser';
 			const stringified = parser.$$instance.stringify(
-				setter({} as unknown as V) as unknown as V,
+				fn({} as unknown as S) as unknown as S,
 			);
 
-			const persister = this.pluginStruct?.persister as PersisterContext<V>;
+			const persister = this.pluginStruct?.persister as PersisterContext<S>;
 			if (typeof persister === 'undefined') throw 'should have least one persister';
 			await persister.$$instance.set({ $$value: stringified });
 
@@ -37,13 +49,13 @@ export class Core<V extends object> {
 		});
 	}
 
-	private async setByValue(value: ValueArg<V>): Promise<void> {
+	private async setByValue(value: SetObject<S>): Promise<void> {
 		return new Promise(async (resolve) => {
-			const parser = this.pluginStruct?.parser as ParserContext<V>;
+			const parser = this.pluginStruct?.parser as ParserContext<S>;
 			if (typeof parser === 'undefined') throw 'should have least one parser';
-			const stringified = parser.$$instance.stringify(value as unknown as V);
+			const stringified = parser.$$instance.stringify(value as unknown as S);
 
-			const persister = this.pluginStruct?.persister as PersisterContext<V>;
+			const persister = this.pluginStruct?.persister as PersisterContext<S>;
 			if (typeof persister === 'undefined') throw 'should have least one persister';
 			await persister.$$instance.set({ $$value: stringified });
 
@@ -51,17 +63,23 @@ export class Core<V extends object> {
 		});
 	}
 
-	async get<K = undefined>(): Promise<V>;
-	async get<K extends keyof V, R = V[K]>(key: K): Promise<R>;
-	async get<K extends keyof V, P extends undefined | K = undefined, R = P extends K ? V[P] : V>(
+	private async setByKeyValue<K extends keyof S, V = SetObjectByKey<S, K>>(key: K, value: V) {
+		return;
+	}
+
+	private async setByKeySetter<K extends keyof S, Fn = SetFnByKey<S, K>>(key: K, fn: Fn) { }
+
+	async get<K = undefined>(): Promise<S>;
+	async get<K extends keyof S, R = S[K]>(key: K): Promise<R>;
+	async get<K extends keyof S, P extends undefined | K = undefined, R = P extends K ? S[P] : S>(
 		prop?: P,
 	): Promise<R> {
 		return new Promise(async (resolve) => {
-			const persister = this.pluginStruct?.persister as PersisterContext<V>;
+			const persister = this.pluginStruct?.persister as PersisterContext<S>;
 			if (typeof persister === 'undefined') throw 'should have least one persister';
 			const raw_value = await persister.$$instance.get({});
 
-			const parser = this.pluginStruct?.parser as ParserContext<V>;
+			const parser = this.pluginStruct?.parser as ParserContext<S>;
 			if (typeof parser === 'undefined') throw 'should have least one parser';
 			const parsed = parser.$$instance.parse(raw_value.$$value);
 
