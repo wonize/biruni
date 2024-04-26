a7a385c1ccc8b1bb700dcba863175d42c680cd32
mikoloism <mikoloism@gmail.com>
2024-04-26 18:28:27 +0330

    build(package.json): add tempalte of `package.json` to `scripts/`

---
+++ scripts/generator/package.mjs
@@ -0,0 +1,59 @@
+const pkg = `
+{
+	"$schema": "https://json.schemastore.org/package",
+	"name": "{{pkgname}}",
+	"type": "module",
+	"version": "0.0.0",
+	"description": "(Al-Biruni) {{pkgdescription}}",
+	"keywords": [
+		"wonize",
+		"biruni",
+		"al-biruni"
+	],
+	"exports": {
+		".": {
+			"import": {
+				"types": "./dist/mod.d.ts",
+				"default": "./dist/mod.mjs"
+			},
+			"require": {
+				"types": "./dist/mod.d.cts",
+				"default": "./dist/mod.cjs"
+			},
+			"default": "./dist/mod.mjs",
+			"types": "./dist/mod.d.ts"
+		},
+		"./*": "./*"
+	},
+	"author": "mikoloism <mikoloism@gmail.com>",
+	"license": "GPL-3.0-only",
+	"homepage": "https://wonize.github.io/biruni/",
+	"bugs": "https://github.com/wonize/biruni/issues",
+	"repository": {
+	  "type": "git",
+	  "url": "git+https://github.com/wonize/biruni.git",
+	  "directory": "{{workspace}}"
+	},
+	"publishConfig": {
+	  "access": "public"
+	},
+	"sideEffects": false,
+	"main": "dist/mod.cjs",
+	"module": "dist/mod.mjs",
+	"types": "dist/mod.d.ts",
+	"files": [
+	  "dist",
+	  "LICENSE",
+	  "README.md",
+	  "package.json"
+	],
+	"packageManager": "pnpm@8.9.0",
+	"engines": {
+	  "node": ">=18"
+	},
+	"scripts": {},
+	"dependencies": {},
+	"devDependencies": {},
+	"peerDependencies": {},
+	"peerDependenciesMeta": {}
+}`;
