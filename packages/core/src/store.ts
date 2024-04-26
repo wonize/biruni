c2767f880422535e41f9f54997ed732f4b8fb412
mikoloism <mikoloism@gmail.com>
2024-04-26 23:14:19 +0330

    feat(core): remap `StoreImpl` as `Store` named export
    remove `store.impl.ts` file and merged with `store.ts`

--- packages/core/src/store.ts
+++ packages/core/src/store.ts
@@ -1,5 +1,18 @@
-import type { StoreGet } from './get';
+import { StoreGet } from './get';
 import type { StoreData } from './globals';
+import type { PluginStruct } from './plugin/struct';
 import type { StoreSet } from './set';
 
-export interface Store<S extends StoreData> extends StoreGet<S>, StoreSet<S> { }
+class StoreImpl<S extends StoreData> extends StoreGet<S> implements Store<S> {
+	public constructor(
+		protected initializer: () => NoInfer<S>,
+		protected pluginStruct: PluginStruct<S>,
+	) {
+		super(pluginStruct);
+		this.set(initializer);
+	}
+}
+
+interface Store<S extends StoreData> extends StoreGet<S>, StoreSet<S> { }
+
+export { StoreImpl as Store };
