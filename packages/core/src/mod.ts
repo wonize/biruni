7639172f3c78184ee23b8c5c159f6fd1412b162f
mikoloism <mikoloism@gmail.com>
2024-04-26 23:15:57 +0330

    feat(core): improve barrel export of `mod.ts` and support `default` export of `StoreImpl` as `Store` named

--- packages/core/src/mod.ts
+++ packages/core/src/mod.ts
@@ -1,6 +1,12 @@
-export * from './context';
-export * from './globals';
-export * from './plugin/mod';
-export * from './store';
-export * from './store.impl';
+export type * from './context';
+export type * from './globals';
+export type * from './parser/parser';
+export type * from './persister/persister';
+export type * from './plugin/mod';
+export type * from './validator/validator';
 
+export type { StoreGetGeneric } from './get';
+export type { StoreSetGeneric } from './set';
+
+export * from './store';
+export { Store as default } from './store';
