4414f2940758cc3d19a244f4f131d1a2d1e6b3d1
mikoloism <mikoloism@gmail.com>
2024-04-27 14:22:59 +0330

    refactor(pkg/factory): improve generic types

--- packages/factory/src/chain.ts
+++ packages/factory/src/chain.ts
@@ -1,6 +1,6 @@
 import type { ExactPlugin, Store, StoreData } from '@biruni/core';
 
-export interface StoreFactoryChain<S extends StoreData> {
-	plug(plugin: ExactPlugin): StoreFactoryChain<S>;
-	init(initializer: () => NoInfer<S>): Store<S>;
+export interface StoreFactoryChain<TData extends StoreData> {
+	plug(plugin: ExactPlugin): StoreFactoryChain<TData>;
+	init(initializer: () => TData): Store<TData>;
 }
--- packages/factory/src/factory.ts
+++ packages/factory/src/factory.ts
@@ -2,39 +2,39 @@ import type { ExactPlugin, PluginStruct, StoreData } from '@biruni/core';
 import Store from '@biruni/core';
 import type { StoreFactoryChain } from './chain';
 
-class StoreFactory<S extends StoreData> implements StoreFactoryChain<S> {
-	readonly #pluginStruct: PluginStruct<S> = {
+class StoreFactory<TData extends StoreData> implements StoreFactoryChain<TData> {
+	readonly #pluginStruct: PluginStruct<TData> = {
 		validator: {},
 		parser: {},
 		persister: {},
-	} as PluginStruct<S>;
+	} as PluginStruct<TData>;
 
-	public constructor(plugin?: ExactPlugin, comingPluginStruct?: PluginStruct<S>) {
+	public constructor(plugin?: ExactPlugin, comingPluginStruct?: PluginStruct<TData>) {
 		const pluginStruct = comingPluginStruct;
 
 		if (typeof plugin !== 'function') return;
 		const pluginContext = plugin();
 		this.#pluginStruct = Object.assign({}, pluginStruct, {
 			[pluginContext.$$type]: pluginContext,
-		}) as PluginStruct<S>;
+		}) as PluginStruct<TData>;
 	}
 
-	public plug(plugin: ExactPlugin): StoreFactoryChain<S> {
-		return new StoreFactory<S>(plugin, this.#pluginStruct);
+	public plug(plugin: ExactPlugin): StoreFactoryChain<TData> {
+		return new StoreFactory<TData>(plugin, this.#pluginStruct);
 	}
 
-	public init(initializer: () => NoInfer<S>): Store<S> {
-		return new Store<S>(initializer, this.#pluginStruct);
+	public init<D extends TData>(initializer: () => D): Store<D> {
+		return new Store<D>(initializer, this.#pluginStruct);
 	}
 }
 
-function defineBiruni<S extends StoreData>(): StoreFactoryChain<S> {
-	return new StoreFactory<S>();
+function defineBiruni<TData extends StoreData>(): StoreFactoryChain<TData> {
+	return new StoreFactory<TData>();
 }
 
 export {
 	StoreFactory as Biruni,
 	StoreFactory,
 	defineBiruni as biruni,
-	defineBiruni,
+	defineBiruni
 };
