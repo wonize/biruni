03ae293c8ffb81b07876d6c2b8a79b52cc69ff45
mikoloism <mikoloism@gmail.com>
2024-04-27 13:47:00 +0330

    ci(scripts): add workspaces to `build.ts` file

--- scripts/build.ts
+++ scripts/build.ts
@@ -4,10 +4,10 @@ import build from './release-helper';
 async function main() {
 	await build('packages/core');
 	await build('packages/built-in');
-	// await build('packages/factory');
-	// await build('packages/biruni');
-	// await build('packages/react');
-	// await build('packages/zod');
+	await build('packages/factory');
+	await build('packages/biruni');
+	await build('packages/react');
+	await build('packages/zod');
 }
 
 main().catch((err: unknown) => {
