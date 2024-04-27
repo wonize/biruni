91800756d1470bbcffcaa8dc37f9c79c598fb403
mikoloism <mikoloism@gmail.com>
2024-04-27 12:37:40 +0330

    ci(scripts): improve `build` script by update the helper function call

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,23 +1,13 @@
 import { default as process } from 'node:process';
-import { build } from 'tsup';
-import { $ } from 'zx';
-import { cjs, dts, esm, umd } from './release-helper';
-
-$.verbose = false
+import build from './release-helper';
 
 async function main() {
-	await $`cd ./packages/core && rm -rf ./dist`
-	await build(esm('packages/core', { entry: ['src/'] }))
-	await build(cjs('packages/core', { entry: ['src/'] }))
-	await build(umd('packages/core', { entry: ['src/mod.ts'], }))
-	await dts('packages/core');
-
-	// keep-order,,
-	//	b('packages/factory'),
-	//	b('packages/built-in'),
-	//	b('packages/biruni'),
-	//	b('packages/react'),
-	//	b('packages/zod'),
+	await build('packages/core');
+	await build('packages/built-in');
+	// await build('packages/factory');
+	// await build('packages/biruni');
+	// await build('packages/react');
+	// await build('packages/zod');
 }
 
 main().catch((err: unknown) => {
