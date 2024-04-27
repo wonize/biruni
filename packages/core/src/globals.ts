7ab9d2d339fdc72be565b6b54f97ed262d33ad64
mikoloism <mikoloism@gmail.com>
2024-04-27 22:09:02 +0330

    feat(core): add `ExtractStoreData` type helper
    replace `Record<string, unknown>` in `globals.ts` for `StoreData`,
    make not work with `interface` defined types! (also the `object` make possible to bad types like instances)
      related to 71cff309

--- packages/core/src/globals.ts
+++ packages/core/src/globals.ts
@@ -1,1 +1,8 @@
-export type StoreData = Record<string, unknown>;
+import type { Store } from "./store";
+
+export type StoreData = object;
+
+export type ExtractStoreData<TStore extends Store<StoreData>> =
+	TStore extends Store<infer TData>
+	? TData
+	: StoreData;
