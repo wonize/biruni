4519bee084218f39d20d4d5ebca8974d10098833
mikoloism <mikoloism@gmail.com>
2024-04-28 13:00:33 +0330

    feat(pkg/core)!: replace `get` overloads of `Interface Function Signature`
    WHY?
    it's cleaner and readable.
    it's make adoptable with external type utilities
    it's make possible to use function signature without duplicate it in class method!
    also possible to use outside of `core` (A.K.A Vue and React integerators)
    
    - remove inheriting pattern (it's not useful more than complexity of coupling)
    - add function type assertion helpers

--- packages/core/src/get.ts
+++ packages/core/src/get.ts
@@ -1,94 +1,112 @@
-import type {
-	FnValue,
-	FnWhole,
-	KeyList,
-	KeyRecord,
-	StoreGet,
-	StoreGetByKeyResult,
-	StoreGetByKeysResult,
-	StoreGetByObjectResult,
-	StoreGetByWholeResult
-} from "./get.interface";
-import type { StoreData } from "./globals";
-import type { ParserContext } from "./parser/context";
-import type { PersisterContext } from "./persister/context";
-import { StoreSet } from "./set";
+import type { RemoveNever, StoreData } from './helpers';
 
-class StoreGetImpl<TData extends StoreData> extends StoreSet<TData> implements StoreGet<TData> {
-	get<K = undefined>(): Promise<StoreGetByWholeResult<TData>>;
-	get<K extends keyof TData>(key: K): Promise<StoreGetByKeyResult<TData, K>>;
-	get<K extends keyof TData, L extends KeyList<TData>>(keys: L): Promise<StoreGetByKeysResult<TData, L>>;
-	get<K extends keyof TData, O extends KeyRecord<TData>>(o: O): Promise<StoreGetByObjectResult<TData, O>>;
-	get<K extends keyof TData, F extends FnValue<TData, K> = FnValue<TData, K>>(key: K, fn: F): Promise<ReturnType<F>>;
-	get<K extends keyof TData, F extends FnWhole<TData>>(fn: F): Promise<ReturnType<F>>;
-	get<
-		K extends keyof TData,
-		P extends KeyList<TData> | KeyRecord<TData> | FnWhole<TData>,
-		R extends
-		P extends KeyList<TData>
-		? StoreGetByKeysResult<TData, P>
-		: P extends KeyRecord<TData>
-		? StoreGetByObjectResult<TData, P>
-		: P extends FnWhole<TData>
-		? ReturnType<P> :
-		never
-		= P extends KeyList<TData>
-		? StoreGetByKeysResult<TData, P>
-		: P extends KeyRecord<TData>
-		? StoreGetByObjectResult<TData, P>
-		: P extends FnWhole<TData>
-		? ReturnType<P> :
-		never
-	>(keyOrKeysOrObjOrFn?: P | undefined, fn?: FnValue<TData, K> | undefined): Promise<R> {
-		return new Promise(async (resolve, reject) => {
-			const persister = this.pluginStruct?.persister as PersisterContext<TData>;
-			if (typeof persister === 'undefined') {
-				return reject('should have least one persister');
-			}
-
-			const raw_value = await persister.$$instance.get({});
-
-			const parser = this.pluginStruct?.parser as ParserContext<TData>;
-			if (typeof parser === 'undefined') {
-				return reject('should have least one parser');
-			}
-
-			const value = parser.$$instance.parse(raw_value.$$value) as Readonly<TData>;
-
-			if (typeof keyOrKeysOrObjOrFn === 'string') {
-				const key = keyOrKeysOrObjOrFn as K;
-				if (typeof fn === 'function') {
-					return resolve(fn(value[key]) as R);
-				}
-				else {
-					return resolve(value[key] as R);
-				}
-			}
-			else if (typeof keyOrKeysOrObjOrFn === 'function') {
-				return resolve(keyOrKeysOrObjOrFn(value) as R);
-			}
-			else if (typeof keyOrKeysOrObjOrFn === 'object') {
-				if (keyOrKeysOrObjOrFn === null) { }
-				else if (keyOrKeysOrObjOrFn instanceof Array) {
-					const result = keyOrKeysOrObjOrFn.reduce((o, key) => {
-						return Object.assign({}, o, { [key]: value[key] });
-					}, {});
-					return resolve(result as R);
-				}
-				else {
-					const result = Object.keys(keyOrKeysOrObjOrFn).reduce((o, key) => {
-						return keyOrKeysOrObjOrFn[key] === true
-							? Object.assign({}, o, { [key]: value[key] })
-							: Object.assign({}, o);
-					}, {});
-					return resolve(result as R);
-				}
-			}
-
-			return reject('cannot resolve selector in <get> method')
-		});
-	}
+interface WholeData<Data extends StoreData> {
+	(): Promise<
+		Readonly<Data>
+	>;
 }
 
-export type { StoreGetGeneric } from './get.interface';
-export { StoreGetImpl as StoreGet };
+interface SingleKey<Data extends StoreData> {
+	<Key extends keyof Data>(key: Key): Promise<
+		Data[Key]
+	>;
+}
+
+interface KeyMapper<Data extends StoreData> {
+	<Key extends keyof Data, Mapper extends SingleKeyMapperFunction<Data, Key>>(key: Key, mapper: Mapper): Promise<
+		ReturnType<Mapper>
+	>;
+}
+
+interface SingleKeyMapperFunction<Data extends StoreData, Key extends keyof Data> {
+	(data: Data[Key] | never): Data[Key] | unknown;
+}
+
+interface Mapper<Data extends StoreData> {
+	<Mapper extends MapperFunction<Data>>(mapper: Mapper): Promise<
+		ReturnType<Mapper>
+	>;
+}
+
+interface MapperFunction<Data extends StoreData> {
+	(data: Readonly<Data> | never): Data | unknown;
+}
+
+interface TruthyKeys<Data extends StoreData> {
+	<KeyObject extends Partial<Record<keyof Data, boolean>>>(keys: KeyObject): Promise<
+		Readonly<
+			RemoveNever<{
+				[SelectedKey in keyof KeyObject]:
+				KeyObject[SelectedKey] extends false
+				? never
+				// @ts-ignore
+				: Data[string & SelectedKey]
+			}>
+		>
+	>
+}
+
+interface KeyList<Data extends StoreData> {
+	<KeyList extends Array<keyof Data>>(keys: Partial<KeyList>): Promise<
+		Readonly<{
+			[SelectedKey in KeyList[number]]: Data[SelectedKey]
+		}>
+	>;
+}
+
+interface Overloads<Data extends StoreData> extends
+	WholeData<Data>,
+	Mapper<Data>,
+	SingleKey<Data>,
+	KeyMapper<Data>,
+	TruthyKeys<Data>,
+	KeyList<Data> { }
+
+export type {
+	KeyList,
+	KeyMapper,
+	Mapper,
+	Overloads,
+	SingleKey,
+	TruthyKeys,
+	WholeData
+};
+
+
+const isWholeData = <Data extends StoreData>(input: unknown): input is (undefined | never | null | void) => {
+	return typeof input === 'undefined' || input === null || Boolean(input) === false;
+}
+
+const isKeyOfData = <Data extends StoreData>(input: unknown): input is keyof Data => {
+	return typeof input === 'string';
+}
+
+const isSingleKeyMapper = <Data extends StoreData>(input: unknown): input is SingleKeyMapperFunction<Data, keyof Data> => {
+	return typeof input === 'function';
+}
+
+const isSingleKey = <Data extends StoreData>(input: unknown): input is (undefined | never | null | void) => {
+	return typeof input === 'undefined' || input === null || Boolean(input) === false;
+}
+
+const isMapper = <Data extends StoreData>(input: unknown): input is MapperFunction<Data> => {
+	return typeof input === 'function';
+}
+
+const isKeyList = <Data extends StoreData>(input: unknown): input is Partial<Array<keyof Data>> => {
+	return typeof input === 'object' && input !== null && ((input instanceof Array) || (Array.isArray(input)));
+}
+
+const isTruthyKeys = <Data extends StoreData>(input: unknown): input is Partial<Record<keyof Data, boolean>> => {
+	return typeof input === 'object' && input !== null;
+}
+
+export {
+	isKeyList,
+	isKeyOfData,
+	isMapper,
+	isSingleKey,
+	isSingleKeyMapper,
+	isTruthyKeys,
+	isWholeData
+};
