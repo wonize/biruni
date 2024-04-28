e24609490a82a0a0a21c71e5967ad245d15ce92c
mikoloism <mikoloism@gmail.com>
2024-04-28 15:31:03 +0330

    feat(pkg/core): extract return types of `get` method overloads to side type

--- packages/core/src/get.ts
+++ packages/core/src/get.ts
@@ -2,57 +2,57 @@ import type { RemoveNever, StoreData } from './helpers';
 
 interface WholeData<Data extends StoreData> {
 	(): Promise<
-		Readonly<Data>
+		WholeDataReturnType<Data>
 	>;
 }
+type WholeDataReturnType<Data extends StoreData> = Readonly<Data>;
 
 interface SingleKey<Data extends StoreData> {
 	<Key extends keyof Data>(key: Key): Promise<
-		Data[Key]
+		SingleKeyReturnType<Data, Key>
 	>;
 }
+type SingleKeyReturnType<Data extends StoreData, Key extends keyof Data> = Data[Key];
 
 interface KeyMapper<Data extends StoreData> {
 	<Key extends keyof Data, Mapper extends SingleKeyMapperFunction<Data, Key>>(key: Key, mapper: Mapper): Promise<
-		ReturnType<Mapper>
+		KeyMapperReturnType<Data, Key, Mapper>
 	>;
 }
-
 interface SingleKeyMapperFunction<Data extends StoreData, Key extends keyof Data> {
 	(data: Data[Key] | never): Data[Key] | unknown;
 }
+type KeyMapperReturnType<Data extends StoreData, Key extends keyof Data, Mapper extends SingleKeyMapperFunction<Data, Key>> = ReturnType<Mapper> | Data[Key] | unknown;
 
 interface Mapper<Data extends StoreData> {
-	<Mapper extends MapperFunction<Data>>(mapper: Mapper): Promise<
-		ReturnType<Mapper>
-	>;
+	<Mapper extends MapperFunction<Data>>(mapper: Mapper): Promise<MapperReturnType<Data, Mapper>>;
 }
-
 interface MapperFunction<Data extends StoreData> {
 	(data: Readonly<Data> | never): Data | unknown;
 }
+type MapperReturnType<Data extends StoreData, Mapper extends MapperFunction<Data>> = ReturnType<Mapper> | Data | unknown;
 
 interface TruthyKeys<Data extends StoreData> {
-	<KeyObject extends Partial<Record<keyof Data, boolean>>>(keys: KeyObject): Promise<
-		Readonly<
-			RemoveNever<{
-				[SelectedKey in keyof KeyObject]:
-				KeyObject[SelectedKey] extends false
-				? never
-				// @ts-ignore
-				: Data[string & SelectedKey]
-			}>
-		>
-	>
-}
-
-interface KeyList<Data extends StoreData> {
-	<KeyList extends Array<keyof Data>>(keys: Partial<KeyList>): Promise<
-		Readonly<{
-			[SelectedKey in KeyList[number]]: Data[SelectedKey]
-		}>
+	<KeyObject extends Record<keyof Data, boolean>>(keys: Partial<KeyObject>): Promise<
+		TruthyKeysReturnType<Data, KeyObject>
 	>;
 }
+type TruthyKeysReturnType<Data extends StoreData, KeyObject extends Partial<Record<keyof Data, boolean>>> = Readonly<
+	RemoveNever<{
+		[SelectedKey in keyof KeyObject]:
+		KeyObject[SelectedKey] extends false
+		? never
+		// @ts-ignore
+		: Data[string & SelectedKey]
+	}>
+>;
+
+interface KeyList<Data extends StoreData> {
+	<KeyList extends Array<keyof Data>>(keys: Partial<KeyList>): Promise<KeyListReturnType<Data, KeyList>>;
+}
+type KeyListReturnType<Data extends StoreData, KeyList extends Partial<Array<keyof Data>>> = Readonly<{
+	[SelectedKey in KeyList extends Partial<Array<infer Key>> ? Key : never]: Data[SelectedKey]
+}>;
 
 interface Overloads<Data extends StoreData> extends
 	WholeData<Data>,
@@ -63,13 +63,8 @@ interface Overloads<Data extends StoreData> extends
 	KeyList<Data> { }
 
 export type {
-	KeyList,
-	KeyMapper,
-	Mapper,
-	Overloads,
-	SingleKey,
-	TruthyKeys,
-	WholeData
+	KeyList, KeyListReturnType, KeyMapper, KeyMapperReturnType, Mapper, MapperReturnType, Overloads,
+	SingleKey, SingleKeyReturnType, TruthyKeys, TruthyKeysReturnType, WholeData, WholeDataReturnType
 };
 
 
