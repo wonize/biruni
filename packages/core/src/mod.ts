a0591524142154f6be791dd9af326c9ea84eff5b
mikoloism <mikoloism@gmail.com>
2024-04-28 15:51:12 +0330

    feat(pkg/core): improve barrel export (`mod.ts`)

--- packages/core/src/mod.ts
+++ packages/core/src/mod.ts
@@ -1,12 +1,10 @@
 export type * from './context';
-export type * from './globals';
+export type * from './helpers';
 export type * from './parser/parser';
 export type * from './persister/persister';
 export type * from './plugin/mod';
 export type * from './validator/validator';
 
-export type { StoreGetGeneric } from './get';
-export type { StoreSetGeneric } from './set';
-
-export * from './store';
-export { Store as default } from './store';
+export * as Getter from './get';
+export * as Setter from './set';
+export { Store, Store as default, type StoreInterface } from './store';
