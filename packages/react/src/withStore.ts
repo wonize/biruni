b2ca59ac4d9ac2baf3a8d12321fb957d456051f0
mikoloism <mikoloism@gmail.com>
2024-04-28 16:03:54 +0330

    refactor(pkg/react): replace extract data geneirc with `ExtractStoreData` helper type
    replace `Record<string, unknown>` with `object` in convention of `types/react`

--- packages/react/src/withStore.ts
+++ packages/react/src/withStore.ts
@@ -1,11 +1,11 @@
-import type { Store, StoreData } from '@biruni/core';
+import type { ExtractStoreData, Store, StoreData } from '@biruni/core';
 import React from 'react';
 import { useStore } from './useStore';
 
 function withStore<
 	TStore extends Store<StoreData>,
-	TProps extends StoreData,
-	TData extends StoreData = TStore extends Store<infer Data> ? Data : StoreData,
+	TProps extends object = object,
+	TData extends StoreData = ExtractStoreData<TStore>,
 	TCleanProps = Omit<TProps, keyof TData>,
 	TNewProps = TCleanProps & Partial<TData>,
 >(Store: TStore, Component: React.ComponentType<TProps & Partial<TNewProps>>) {
@@ -33,5 +33,5 @@ export { withStore as withBiruni, withStore };
 
 export type WithStore<
 	S extends Store<StoreData>,
-	P extends Record<string, unknown> = Record<string, unknown>> =
+	P extends object = object> =
 	S extends Store<infer Data> ? Omit<P, keyof Data> & Partial<Data> : P;
