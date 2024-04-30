734d7315b7e653b469630eca6a9fbb32eebc5add
mikoloism <mikoloism@gmail.com>
2024-04-30 19:44:25 +0330

    feat(pkg/zod): support generic type for instance

--- packages/zod/src/mod.ts
+++ packages/zod/src/mod.ts
@@ -1,15 +1,22 @@
 import type { Plugin } from '@biruni/core';
+import { StoreData } from '@biruni/core/helpers';
+import { Validator } from '@biruni/core/validator';
 import type { ZodSchema } from 'zod';
 
-const zod: Plugin<[schema: ZodSchema]> = (schema: ZodSchema) => {
-	return function () {
+class ZodValidator<Data extends StoreData> implements Validator<Data> {
+	public constructor(private schema: ZodSchema) { }
+
+	public validate<CustomData extends Data>(data: CustomData): boolean {
+		return Boolean(this.schema.parse(data));
+	}
+}
+
+const zod = (schema: ZodSchema): Plugin.Function => {
+	return function <Data extends StoreData>() {
+		const $$instance = new ZodValidator<Data>(schema);
 		return {
 			$$type: 'validator',
-			$$instance: {
-				validate(data) {
-					return !!schema.parse(data);
-				},
-			},
+			$$instance: $$instance,
 		};
 	};
 }
