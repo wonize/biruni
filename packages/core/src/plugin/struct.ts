1f05296454a354e1cada4dcd97cbac64e6f1a610
mikoloism <mikoloism@gmail.com>
2024-04-30 12:00:18 +0330

    feat(pkg/core): update plugin struct type of context
    instead useing `CoreContext` which mean `unknown` exactly, replaced by real named context

--- packages/core/src/plugin/struct.ts
+++ packages/core/src/plugin/struct.ts
@@ -1,8 +1,11 @@
-import type { CoreContext } from '../context';
-import type { StoreData } from '../helpers';
+import type { CoreContext } from '@/context';
+import type { StoreData } from '@/helpers';
+import type * as Synchronizer from '@/synchronizer';
+import type * as Validator from '@/validator';
 
-export type PluginStruct<TData extends StoreData> = {
-	validator: CoreContext<TData>;
-	parser: CoreContext<TData>;
-	persister: CoreContext<TData>;
+export type PluginStruct<Data extends StoreData> = {
+	synchronizer: Synchronizer.Context<Data>;
+	validator: Validator.Context<Data>;
+	persister: CoreContext<Data>;
+	parser: CoreContext<Data>;
 }
