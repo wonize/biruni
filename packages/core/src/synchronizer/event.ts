02231de701b93e0943a44db67bfb3f541b2ac35b
mikoloism <mikoloism@gmail.com>
2024-04-30 22:35:05 +0330

    feat(pkg/core): add `EventMap` as `Record` which keys include event names

--- packages/core/src/synchronizer/event.ts
+++ packages/core/src/synchronizer/event.ts
@@ -2,3 +2,4 @@ export type EventBase = 'change' /* TODO: //| 'applySet' | 'resolveGet' */;
 export type EventPrefix = 'pre' | 'post';
 export type EventName = `${EventPrefix}${Capitalize<EventBase>}`;
 export type EventMethod = `on${Capitalize<EventName>}`;
+export type EventMap = Record<EventName, unknown[]>;
