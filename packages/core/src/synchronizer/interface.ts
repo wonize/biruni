37cdf89942f4531e482165baf912dcd4428c9c0e
mikoloism <mikoloism@gmail.com>
2024-04-30 22:35:50 +0330

    style(pkg/core): format `synchronizer/interface.ts`

--- packages/core/src/synchronizer/interface.ts
+++ packages/core/src/synchronizer/interface.ts
@@ -4,21 +4,14 @@ import type { ListenerFunction } from "./listener";
 import type { Payload } from './payload';
 
 export interface Synchronizer<Data extends StoreData> {
-	on<
-		Event extends EventName = EventName,
-		Keys extends KeyDiff<Data> = KeyDiff<Data>,
-		Diffs extends DataDiff<Data, Keys> = DataDiff<Data, Keys>
-	>(payload: {
+	on<Event extends EventName, Keys extends KeyDiff<Data>, Diffs extends DataDiff<Data, Keys>>(payload: {
 		$$event: Event,
 		$$listener: ListenerFunction<Data, NoInfer<Event>, Keys, Diffs>
 	}): void;
 
-	emit<
-		Event extends EventName,
-		Keys extends KeyDiff<Data>,
-		Diffs extends DataDiff<Data, Keys>
-	>(payload: {
+	emit<Event extends EventName, Keys extends KeyDiff<Data>, Diffs extends DataDiff<Data, Keys>>(payload: {
 		$$event: Event,
 		$$payload: Payload<Data, NoInfer<Event>, Keys, Diffs>,
-	}): void;
+	}
+	): void;
 }
