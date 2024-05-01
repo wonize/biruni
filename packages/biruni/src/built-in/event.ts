4a3d2db6fbe4b3f07a771eac9887f0e1e2aa33fb
mikoloism <mikoloism@gmail.com>
2024-05-01 16:07:38 +0330

    fix(pkg/biruni): add missing exports for `EventEmitterPlugin`
    - add `exports` field in `package.json`
    - add to `mod.ts`
    - add it's own directory

---
+++ packages/biruni/src/built-in/event.ts
@@ -0,0 +1,1 @@
+export * from '@biruni/built-in/event';
--- packages/biruni/src/mod.ts
+++ packages/biruni/src/mod.ts
@@ -1,3 +1,3 @@
-export { JsonPlugin, LocalStoragePlugin, json, localstorage } from '@biruni/built-in';
+export { JsonPlugin, LocalStoragePlugin, json, localstorage, event, EventEmitterPlugin } from '@biruni/built-in';
 export { Store, type StoreInterface } from '@biruni/core';
 export { Biruni, biruni, biruni as default, defineBiruni } from '@biruni/factory';
