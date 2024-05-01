5bd72ad7213d119b99e01bcce8f86a9633114c5c
mikoloism <mikoloism@gmail.com>
2024-05-01 16:37:55 +0330

    docs(examples): implement basic store in `react` and make sidebyside comparing view in `App.tsx`

--- examples/react-example/src/App.tsx
+++ examples/react-example/src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { CounterOutput as BiruniOutput } from './BiruniHook';
+import { CounterOuput as ReactOutput } from './ReactHook';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function () {
+	return (
+		<div style={{ flexDirection: 'row', display: 'flex', gap: '10vw' }}>
+			<div style={{ maxWidth: '45vw' }}>
+				<ReactOutput />
+			</div>
+			<hr />
+			<div style={{ maxWidth: '45vw' }}>
+				<BiruniOutput />
+			</div>
+		</div>
+	)
 }
-
-export default App
