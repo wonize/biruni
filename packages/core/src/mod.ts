c568a498b90e1a6c6af694deacec072607107c91
mikoloism <mikoloism@gmail.com>
2024-05-05 10:07:28 +0330

    refactor(pkg/core): update barrel exports
    rename `On` to `Listener`
    add `type` prefix for `get-sync`

--- packages/core/src/mod.ts
+++ packages/core/src/mod.ts
@@ -3,7 +3,7 @@ export type * as Plugin from './plugin/mod';
 export type * as Event from './synchronizer/event';
 
 export * as Getter from './get';
-export * as GetterSync from './get-sync';
-export type * as On from './on';
+export type * as GetterSync from './get-sync';
+export type * as Listener from './listener';
 export * as Setter from './set';
 export { Store, Store as default, type StoreInterface } from './store';
