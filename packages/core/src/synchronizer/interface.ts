939cfd803aafd58efd1efee3f9cedbf8db117063
mikoloism <mikoloism@gmail.com>
2024-05-05 10:05:46 +0330

    refactor(pkg/core): rename `on` to `addListener` in `Synchronizer` interface
    add `removeListener` to `Synchronizer` interface

--- packages/core/src/synchronizer/interface.ts
+++ packages/core/src/synchronizer/interface.ts
@@ -4,7 +4,12 @@ import type { ListenerFunction } from "./listener";
 import type { Payload } from './payload';
 
 export interface Synchronizer<Data extends StoreData> {
-	on<Event extends EventName, Keys extends KeyDiff<Data>, Diffs extends DataDiff<Data, Keys>>(payload: {
+	addListener<Event extends EventName, Keys extends KeyDiff<Data>, Diffs extends DataDiff<Data, Keys>>(payload: {
+		$$event: Event,
+		$$listener: ListenerFunction<Data, NoInfer<Event>, Keys, Diffs>
+	}): void;
+
+	removeListener<Event extends EventName, Keys extends KeyDiff<Data>, Diffs extends DataDiff<Data, Keys>>(payload: {
 		$$event: Event,
 		$$listener: ListenerFunction<Data, NoInfer<Event>, Keys, Diffs>
 	}): void;
