ce44d4a45a548ef2dcfcea1186eb21f840fde647
mikoloism <mikoloism@gmail.com>
2024-05-05 18:45:12 +0330

    feat(pkg/core): add `isEmptyObject` helper

--- packages/core/src/helpers/mod.ts
+++ packages/core/src/helpers/mod.ts
@@ -6,3 +6,8 @@ export type * from './type-utility';
 export function shouldFreshInitializing(source: object, target: object): boolean {
 	return JSON.stringify({ input: source }) !== JSON.stringify({ input: target });
 }
+
+interface EmptyObject { }
+export const isEmptyObject = (obj: object): obj is EmptyObject => {
+	return Object.keys(obj ?? {}).length === 0;
+}
