879eb4b48fef514f2688cdd086d7a1f7ea4159cd
mikoloism <mikoloism@gmail.com>
2024-05-07 16:01:53 +0330

    feat(pkg/core): implement pre-requires of `by-path` setter and getter

---
+++ packages/core/src/get/by-path-value.ts
@@ -0,0 +1,27 @@
+import { getProperty } from 'dot-prop';
+import { clone, type StoreData } from "../helpers/mod.ts";
+import type * as Path from "../helpers/path-key.ts";
+
+interface ByPathValue<Data extends StoreData> {
+	<TPath extends Path.From<Data>>(path: TPath): Promise<
+		Path.At<Data, NoInfer<TPath>>
+	>;
+}
+
+const isByPath = <Data extends StoreData>(input: unknown): input is Path.From<Data> => {
+	return typeof input === 'string' && input.indexOf('.') !== -1;
+}
+
+function getByPath<
+	TData extends StoreData,
+	TPath extends Path.From<NoInfer<TData>, NoInfer<TSep>>,
+	TValue extends Path.At<NoInfer<TData>, NoInfer<TPath>, NoInfer<TSep>>,
+	TSep extends Path.Separator = '.',
+>(data: TData, path: TPath, separator?: TSep): TValue {
+	const $data = clone(data);
+	const $path = separator ? path.toString().replace(RegExp(separator, 'g'), '.') : path.toString();
+	return getProperty($data, $path)! satisfies TValue;
+}
+
+export { getByPath, isByPath };
+export type { ByPathValue };
---
+++ packages/core/src/set/by-path-value.ts
@@ -0,0 +1,25 @@
+import { setProperty } from 'dot-prop';
+import { clone, type StoreData } from "../helpers/mod.ts";
+import type * as Path from "../helpers/path-key.ts";
+
+interface ByPathValue<Data extends StoreData> {
+	<TPath extends Path.From<Data>, Value extends Path.At<Data, TPath>>(path: TPath, value: Value): Promise<void>;
+}
+
+const isByPath = <Data extends StoreData>(input: unknown): input is Path.From<Data> => {
+	return typeof input === 'string' && input.indexOf('.') !== -1;
+}
+
+function setByPath<
+	TData extends StoreData,
+	TPath extends Path.From<NoInfer<TData>, NoInfer<TSep>>,
+	TValue extends Path.At<NoInfer<TData>, NoInfer<TPath>, NoInfer<TSep>>,
+	TSep extends Path.Separator = '.',
+>(data: TData, path: TPath, value: TValue, separator?: TSep): TData {
+	const $data = clone(data);
+	const $path = separator ? path.toString().replace(RegExp(separator, 'g'), '.') : path.toString();
+	return clone(setProperty($data, $path, value));
+}
+
+export { isByPath, setByPath };
+export type { ByPathValue };
