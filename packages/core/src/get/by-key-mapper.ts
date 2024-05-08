076f935bd4f268f3aa2a47d13ed92e44357276bb
mikoloism <mikoloism@gmail.com>
2024-05-08 17:57:24 +0330

    refactor(pkg/core): rename `by-key-mapper`

--- packages/core/src/get/by-key-mapper.ts
+++ packages/core/src/get/by-key-mapper.ts
@@ -1,26 +1,27 @@
 import type { StoreData } from '../helpers/mod.ts';
 
-interface KeyMapper<Data extends StoreData> {
-	<Key extends keyof Data, Mapper extends SingleKeyMapperFunction<Data, Key>>(key: Key, mapper: Mapper): Promise<
-		KeyMapperReturnType<Data, Key, Mapper>
-	>;
+interface ByKeyMapper<Data extends StoreData> {
+	<Key extends keyof Data, Mapper extends ByKeyMapperFunction<Data, Key>>(
+		key: Key,
+		mapper: Mapper,
+	): Promise<ByKeyMapperReturnType<Data, Key, Mapper>>;
 }
 
-type KeyMapperReturnType<Data extends StoreData, Key extends keyof Data, Mapper extends SingleKeyMapperFunction<Data, Key>> = ReturnType<Mapper> | Data[Key] | unknown;
+type ByKeyMapperReturnType<
+	Data extends StoreData,
+	Key extends keyof Data,
+	Mapper extends ByKeyMapperFunction<Data, Key>,
+> = ReturnType<Mapper> | Data[Key] | unknown;
 
-
-interface SingleKeyMapperFunction<Data extends StoreData, Key extends keyof Data> {
+interface ByKeyMapperFunction<Data extends StoreData, Key extends keyof Data> {
 	(data: Data[Key] | never): Data[Key] | unknown;
 }
 
-
-const isSingleKeyMapper = <Data extends StoreData>(input: unknown): input is SingleKeyMapperFunction<Data, keyof Data> => {
+const isByKeyMapper = <Data extends StoreData>(
+	input: unknown,
+): input is ByKeyMapperFunction<Data, keyof Data> => {
 	return typeof input === 'function';
-}
-
-export {
-	isSingleKeyMapper,
-	type KeyMapper,
-	type KeyMapperReturnType,
-	type SingleKeyMapperFunction
 };
+
+export { isByKeyMapper };
+export type { ByKeyMapper, ByKeyMapperFunction, ByKeyMapperReturnType };
