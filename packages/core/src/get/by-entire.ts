caf29cb99ce0302732b9f874cfbb4adb7c0e236d
mikoloism <mikoloism@gmail.com>
2024-05-08 18:00:56 +0330

    style(pkg/core): format `by-entire` file

--- packages/core/src/get/by-entire.ts
+++ packages/core/src/get/by-entire.ts
@@ -1,27 +1,20 @@
 import { clone, type StoreData } from '../helpers/mod.ts';
 
 interface ByEntire<Data extends StoreData> {
-	(): Promise<
-		ByEntireReturnType<Data>
-	>;
+	(): Promise<ByEntireReturnType<Data>>;
 }
 
 type ByEntireReturnType<Data extends StoreData> = Readonly<Data>;
 
-const isByEntire = <Data extends StoreData>(input: unknown): input is (undefined | never | null | void) => {
+const isByEntire = <Data extends StoreData>(
+	input: unknown,
+): input is undefined | never | null | void => {
 	return typeof input === 'undefined' || input === null || Boolean(input) === false;
-}
+};
 
 function getByEntire<Data extends StoreData>(data: Data): ByEntireReturnType<Data> {
 	return clone(data);
 }
 
-export {
-	getByEntire,
-	isByEntire
-};
-
-export type {
-	ByEntire,
-	ByEntireReturnType
-};
+export { getByEntire, isByEntire };
+export type { ByEntire, ByEntireReturnType };
