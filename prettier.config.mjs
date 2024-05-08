d89799736ae9e9ab66e2a855459c9b8fd4808b8a
mikoloism <mikoloism@gmail.com>
2024-05-08 17:48:42 +0330

    chore(formatter): install `prettier` and configure in `prettier.config.mjs` and `.vscode/settings.json`
    set default formatter for `js{x},ts{x},md` file extendions

---
+++ prettier.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+export default {
+	arrowParens: 'always',
+	bracketSameLine: true,
+	bracketSpacing: true,
+	singleQuote: true,
+	quoteProps: 'as-needed',
+	jsxSingleQuote: false,
+	htmlWhitespaceSensitivity: 'css',
+	semi: true,
+	printWidth: 80,
+	trailingComma: 'es5',
+	tabWidth: 4,
+	useTabs: true,
+	endOfLine: 'lf',
+};
