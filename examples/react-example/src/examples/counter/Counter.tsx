abab98d5d36d148fdd31dbc43bfc19f747e769bb
mikoloism <mikoloism@gmail.com>
2024-05-10 14:49:57 +0330

    docs(react-example): sync with new plugin hooks

--- examples/react-example/src/examples/counter/Counter.tsx
+++ examples/react-example/src/examples/counter/Counter.tsx
@@ -1,22 +1,23 @@
-import { useStore } from "@biruni/react";
+import { useStore } from '@biruni/react';
 import { biruni, event, json, localstorage } from 'biruni';
 import './Counter.css';
 
-const CounterStore = biruni()
+const CounterStore = biruni('biruni-example-counter')
 	.plug(event())
 	.plug(json())
-	.plug(localstorage('biruni-example-counter'))
-	.init(() => ({ count: 0 }))
+	.plug(localstorage())
+	.init(() => ({ count: 0 }));
 
 const Counter = () => {
 	const counter = useStore(CounterStore);
 
 	const reset = () => counter.set('count', 0);
 	const increase = () => counter.set('count', (count) => count + 1);
-	const decrease = () => counter.set('count', count => {
-		if (count <= 0) return 0;
-		return count - 1;
-	});
+	const decrease = () =>
+		counter.set('count', (count) => {
+			if (count <= 0) return 0;
+			return count - 1;
+		});
 
 	return (
 		<div className="counter">
@@ -25,12 +26,18 @@ const Counter = () => {
 				<p className="snippet"></p>
 			</div>
 			<div className="controls">
-				<button className="increase" onClick={increase}>Increase</button>
-				<button className="decrease" onClick={decrease}>Decrease</button>
-				<button className="reset" onClick={reset}>Reset</button>
+				<button className="increase" onClick={increase}>
+					Increase
+				</button>
+				<button className="decrease" onClick={decrease}>
+					Decrease
+				</button>
+				<button className="reset" onClick={reset}>
+					Reset
+				</button>
 			</div>
 		</div>
-	)
-}
+	);
+};
 
 export { Counter };
