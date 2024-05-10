d4706209117543a93e6ecc5f8d67d46d6a13702a
mikoloism <mikoloism@gmail.com>
2024-05-10 14:08:20 +0330

    style(pkg/core): remove `.ts` extension

--- packages/core/src/synchronizer/listener.ts
+++ packages/core/src/synchronizer/listener.ts
@@ -1,6 +1,6 @@
-import type { DataDiff, KeyDiff, StoreData } from '../helpers/mod.ts';
-import type { EventName } from './event.ts';
-import type { Payload } from './payload.ts';
+import type { DataDiff, KeyDiff, StoreData } from '../helpers/mod';
+import type { EventName } from './event';
+import type { Payload } from './payload';
 
 interface ListenerFunction<Data extends StoreData> {
 	<Event extends EventName, Keys extends KeyDiff<Data>, Diffs extends DataDiff<Data, Keys>>(
