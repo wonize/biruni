e4aadea6c0e3d01c7b4d1452d339dd1fdda3ad61
mikoloism <mikoloism@gmail.com>
2024-05-10 08:25:52 +0330

    fix(pkg/core): remove additional generic parameter to be sync with new listener function

--- packages/core/src/synchronizer/interface.ts
+++ packages/core/src/synchronizer/interface.ts
@@ -1,22 +1,33 @@
-import type { DataDiff, KeyDiff, StoreData } from "../helpers/mod";
+import type { DataDiff, KeyDiff, StoreData } from '../helpers/mod';
 import type { EventName } from './event';
-import type { ListenerFunction } from "./listener";
+import type { ListenerFunction } from './listener';
 import type { Payload } from './payload';
 
 export interface Synchronizer<Data extends StoreData> {
-	addListener<Event extends EventName, Keys extends KeyDiff<Data>, Diffs extends DataDiff<Data, Keys>>(payload: {
-		$$event: Event,
-		$$listener: ListenerFunction<Data, NoInfer<Event>, Keys, Diffs>
+	addListener<
+		Event extends EventName,
+		Keys extends KeyDiff<Data>,
+		Diffs extends DataDiff<Data, Keys>,
+	>(payload: {
+		$$event: Event;
+		$$listener: ListenerFunction<Data>;
 	}): void;
 
-	removeListener<Event extends EventName, Keys extends KeyDiff<Data>, Diffs extends DataDiff<Data, Keys>>(payload: {
-		$$event: Event,
-		$$listener: ListenerFunction<Data, NoInfer<Event>, Keys, Diffs>
+	removeListener<
+		Event extends EventName,
+		Keys extends KeyDiff<Data>,
+		Diffs extends DataDiff<Data, Keys>,
+	>(payload: {
+		$$event: Event;
+		$$listener: ListenerFunction<Data>;
 	}): void;
 
-	emit<Event extends EventName, Keys extends KeyDiff<Data>, Diffs extends DataDiff<Data, Keys>>(payload: {
-		$$event: Event,
-		$$payload: Payload<Data, NoInfer<Event>, Keys, Diffs>,
-	}
-	): void;
+	emit<
+		Event extends EventName,
+		Keys extends KeyDiff<Data>,
+		Diffs extends DataDiff<Data, Keys>,
+	>(payload: {
+		$$event: Event;
+		$$payload: Payload<Data, NoInfer<Event>, Keys, Diffs>;
+	}): void;
 }
--- packages/core/src/synchronizer/payload.ts
+++ packages/core/src/synchronizer/payload.ts
@@ -1,11 +1,11 @@
-import type { DataDiff, KeyDiff, StoreData } from "../helpers/mod";
-import type { EventName } from "./event";
+import type { DataDiff, KeyDiff, StoreData } from '../helpers/mod';
+import type { EventName } from './event';
 
 export interface Payload<
 	Data extends StoreData,
 	Event extends EventName,
 	Keys extends KeyDiff<Data>,
-	Diffs extends DataDiff<Data, Keys>
+	Diffs extends DataDiff<Data, Keys>,
 > {
 	event: Event;
 	oldData: Data;
