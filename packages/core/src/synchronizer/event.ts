f809027fce1e22055fd54dce9c174e940b198651
mikoloism <mikoloism@gmail.com>
2024-05-10 07:46:27 +0330

    feat(pkg/core): drop support prefix for events name

--- packages/core/src/synchronizer/event.ts
+++ packages/core/src/synchronizer/event.ts
@@ -1,5 +1,5 @@
-export type EventBase = 'change' /* TODO: //| 'applySet' | 'resolveGet' */;
+export type EventBase = 'change';
 export type EventPrefix = 'pre' | 'post';
-export type EventName = `${EventPrefix}${Capitalize<EventBase>}`;
+export type EventName = EventBase;
 export type EventMethod = `on${Capitalize<EventName>}`;
 export type EventMap = Record<EventName, unknown[]>;
