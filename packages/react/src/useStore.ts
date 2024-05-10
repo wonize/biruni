c56d117928578cd5cb4b8bf84d921fd30c9a72db
mikoloism <mikoloism@gmail.com>
2024-05-10 14:34:11 +0330

    fix(pkg/react): re-implement `useStore` by new plugin hook feature

--- packages/react/src/useStore.ts
+++ packages/react/src/useStore.ts
@@ -7,55 +7,47 @@ function useStore<
 	TStore extends Store<any> = Store<TData>,
 	Data extends TData = ExtractStoreData<TStore>,
 >(store: TStore): UseStore<NoInfer<Data>> {
-	const [data, setData] = React.useState<NoInfer<Data>>(store.initialData);
+	const [data, setData] = React.useState<NoInfer<Data>>(store.data);
 
 	React.useEffect(() => {
-		store.on('postChange', function onPostChange(event) {
+		store.addListener('change', function onPostChange(event) {
 			setData(() => event.newData);
 		});
 	}, []);
 
-	const $$store = React.useMemo<UseStore<NoInfer<Data>>>(
-		() => ({
-			set: store.set,
-			get: (input1?: unknown, input2?: unknown): any => {
-
-				if (Getter.isWholeData(input1)) {
-					return data;
-				} else if (Getter.isKeyOfData(input1)) {
-					if (Getter.isSingleKeyMapper(input2)) {
-						return input2(data[input1]);
-					} else if (Getter.isSingleKey(input2)) {
-						return data[input1]
-					} else { }
-				} else if (Getter.isMapper(input1)) {
-					return input1(data);
-				} else if (Getter.isKeyList(input1)) {
-					const filtered_data = (input1 as unknown as Array<keyof TData>).reduce((filtered_pairs, key) => {
-						return Object.assign({}, filtered_pairs, { [key]: data[key] });
-					}, {});
-					type Result = GetterSync.KeyListReturnType<TData, typeof input1>;
-					return filtered_data as unknown as Result;
-				} else if (Getter.isTruthyKeys(input1)) {
-					const filtered_data = Object.keys(input1).reduce((filtered_pairs, key) => {
-						return (input1 as unknown as Parameters<GetterSync.TruthyKeys<TData>>[0])[key as keyof TData] === true
-							? Object.assign({}, filtered_pairs, { [key]: data[key as keyof TData] })
-							: Object.assign({}, filtered_pairs);
-					}, {});
-					type Result = GetterSync.TruthyKeysReturnType<TData, typeof input1>;
-					return filtered_data as unknown as Result;
-				} else { }
-			}
-		} satisfies UseStore<NoInfer<Data>>),
-		[data],
+	const localStore = React.useMemo<UseStore<NoInfer<Data>>>(
+		() =>
+			({
+				set: store.set,
+				get: (first?: unknown, second?: unknown): any => {
+					if (Getter.isByEntire(first)) {
+						return Getter.getByEntire(data);
+					} else if (Getter.isKeyOfData(first)) {
+						if (Getter.isByKeyMapper(second)) {
+							return Getter.getByKeyMapper(data, first, second);
+						} else if (Getter.isByKey(second)) {
+							return Getter.getByKey(data, first);
+						} else {
+						}
+					} else if (Getter.isByMapper(first)) {
+						return Getter.getByMapper(data, first);
+					} else if (Getter.isByKeys(first)) {
+						return Getter.getByKeys(data, first);
+					} else if (Getter.isByTruthy(first)) {
+						return Getter.getByTruthy(data, first);
+					} else {
+					}
+				},
+			}) satisfies UseStore<NoInfer<Data>>,
+		[data]
 	);
 
-	return $$store;
+	return localStore;
 }
 
 interface UseStore<Data extends StoreData> {
-	set: Setter.Overloads<Data>,
-	get: GetterSync.Overloads<Data>,
+	set: Setter.Overloads<Data>;
+	get: GetterSync.Overloads<Data>;
 }
 
 export { useStore };
