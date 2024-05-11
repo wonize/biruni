be46cfdc3e439b716848f0c16725bdb574b8915f
mikoloism <mikoloism@gmail.com>
2024-05-11 14:34:27 +0330

    feat(pkg/built-in): add alias export for `basic localStorage`

--- packages/built-in/src/mod.ts
+++ packages/built-in/src/mod.ts
@@ -1,4 +1,8 @@
-export { BasicLocalStorage, BasicLocalStorage as default } from './built-in';
+export {
+	BasicLocalStorage,
+	BasicLocalStorage as recommended,
+	BasicLocalStorage as default,
+} from './built-in';
 export { EventEmitterPlugin, event } from './event/mod';
 export { JsonPlugin, json } from './json/mod';
 export { LocalStoragePlugin, localstorage } from './localstorage/mod';
