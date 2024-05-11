943174327226b1531c5889edfccfcfaedb9769c5
mikoloism <mikoloism@gmail.com>
2024-05-11 13:01:41 +0330

    refactor(pkg/core): update barrel exports and `exports` field in `package.json`

--- packages/core/src/mod.ts
+++ packages/core/src/mod.ts
@@ -1,9 +1,9 @@
-export type * as Context from './context';
+export type * as Context from './plugin/context';
 export type * as Plugin from './plugin/mod';
-export type * as Event from './synchronizer/event';
 
 export type * as GetterSync from './get-sync';
 export * as Getter from './get/mod';
+export type * as Event from './listener/event';
 export type * as Listener from './listener/mod';
 export * as Setter from './set/mod';
 export { Store, Store as default } from './store';
