29d44a25b6dc29a09ab2f60670789238cbe80fbb
mikoloism <mikoloism@gmail.com>
2024-05-12 22:55:14 +0330

    chore(eslint): replace manual hard code flat config file instead using custom function

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from '@repo/configs/eslint.mjs';
+import eslint from '@eslint/js';
+import prettier from 'eslint-plugin-prettier/recommended';
+import * as path from 'node:path';
+import * as process from 'node:process';
+import tseslint from 'typescript-eslint';
+import { withFiles } from '@repo/configs/eslint';
 
-export default defineConfig({
-	base: 'recommended',
-	typescript: true,
-	prettier: true,
-});
+const project = path.resolve(process.cwd(), 'tsconfig.json');
+
+export default tseslint.config(
+	...withFiles(eslint.configs.recommended),
+	...withFiles(prettier),
+	...withFiles(tseslint.configs.strict),
+	...withFiles(tseslint.configs.stylistic),
+	...withFiles({
+		plugins: { 'typescript-eslint': tseslint.plugin },
+		languageOptions: {
+			parserOptions: {
+				project,
+				parser: tseslint.parser,
+				sourceType: 'module',
+			},
+		},
+		rules: {
+			'@typescript-eslint/prefer-function-type': 'off',
+		},
+	})
+);
