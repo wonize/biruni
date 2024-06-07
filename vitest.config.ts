4589da8450349dd4e7e87086b27e06ac6e32d87f
mikoloism <mikoloism@gmail.com>
2024-06-07 20:24:11 +0330

    style: format `vitest.config.ts`

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,17 +2,17 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [
-    tsconfigPaths({
-      projects: ['tests/tsconfig.json'],
-    }),
-  ],
-  test: {
-    name: 'unit',
-    globals: true,
-    environment: 'jsdom',
-    mockReset: false,
-    setupFiles: ['./tests/__setups__/localstorage.js'],
-    exclude: configDefaults.exclude.concat('_ignored_'),
-  },
+	plugins: [
+		tsconfigPaths({
+			projects: ['tests/tsconfig.json'],
+		}),
+	],
+	test: {
+		name: 'unit',
+		globals: true,
+		environment: 'jsdom',
+		mockReset: false,
+		setupFiles: ['./tests/__setups__/localstorage.js'],
+		exclude: configDefaults.exclude.concat('_ignored_'),
+	},
 });
