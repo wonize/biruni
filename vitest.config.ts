9f5e60b27f428314e3218db514933457cde6a746
mikoloism <mikoloism@gmail.com>
2024-06-10 11:59:09 +0330

    chore(config): update `vitest.config.ts`
    remove related options to `tests/` directory in root

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,19 +1,13 @@
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { defineConfig, configDefaults } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [
-		tsconfigPaths({
-			ignoreConfigErrors: true,
-			projects: ['./tests/tsconfig.json', './tsconfig.json'],
-		}),
-	],
+	plugins: [tsconfigPaths()],
 	test: {
 		name: 'unit',
 		globals: true,
 		environment: 'jsdom',
 		mockReset: false,
-		setupFiles: ['./tests/__setups__/localstorage.js'],
 		exclude: configDefaults.exclude.concat('_ignored_'),
 	},
 });
