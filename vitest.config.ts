8e6a8628b4e43c04a06f280d81e9d72ecbf89785
mikoloism <mikoloism@gmail.com>
2024-06-13 16:56:07 +0330

    chore(config): pre-config coverage in `vitest`
    configs is commented currently

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,12 +2,17 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [tsconfigPaths()],
-	test: {
-		name: 'unit',
-		globals: true,
-		environment: 'jsdom',
-		mockReset: false,
-		exclude: configDefaults.exclude.concat('_ignored_'),
-	},
+  plugins: [tsconfigPaths()],
+  test: {
+    name: 'unit',
+    globals: true,
+    environment: 'jsdom',
+    mockReset: false,
+    exclude: configDefaults.exclude.concat('_ignored_'),
+    /* coverage: {
+       "provider": "v8",
+       "reporter": ["html", "json", "text", "clover"],
+       // vitest run --coverage
+     } */
+  },
 });
