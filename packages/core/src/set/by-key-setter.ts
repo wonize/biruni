ce2fdb660c3999d391ff7e7d58550b43c694f3e5
mikoloism <mikoloism@gmail.com>
2024-06-13 16:57:12 +0330

    feat(pkg/core): support path-keys in `set/by-key-setter` helper
    add typescript generic paramter
    add helper function implementation

--- packages/core/src/set/by-key-setter.ts
+++ packages/core/src/set/by-key-setter.ts
@@ -1,14 +1,16 @@
+import { getProperty, setProperty } from 'dot-prop';
 import { clone, type StoreData } from '../helpers/mod';
+import type { Path } from '../path/mod';
 
 interface SetByKeySetter<Data extends StoreData> {
-	<Key extends keyof Data, Setter extends SetByKeySetterFunction<Data[Key], Data>>(
+	<Key extends Path.From<Data>, Setter extends SetByKeySetterFunction<Path.At<Data, Key>, Data>>(
 		key: Key,
 		setter: Setter
 	): Promise<void>;
 }
 
 interface SetByKeySetterFunction<
-	Value extends Data[keyof Data],
+	Value extends Path.At<Data, Path.From<Data>>,
 	Data extends StoreData = StoreData,
 > {
 	(value: Readonly<Value>): Value;
@@ -16,18 +18,22 @@ interface SetByKeySetterFunction<
 
 const isByKeySetter = <Data extends StoreData>(
 	input: unknown
-): input is SetByKeySetterFunction<Data[keyof Data], Data> => {
+): input is SetByKeySetterFunction<Path.At<Data, Path.From<Data>>, Data> => {
 	return typeof input === 'function';
 };
 
 function setByKeySetter<
 	Data extends StoreData,
-	Key extends keyof Data,
-	Setter extends SetByKeySetterFunction<Data[Key], Data>,
+	Key extends Path.From<Data>,
+	Setter extends SetByKeySetterFunction<Path.At<Data, Key>, Data>,
 >(data: Data, key: Key, setter: Setter): Data {
 	const clonedInputData = clone(data);
-	const value = clonedInputData[key];
-	const outputData = Object.assign({}, clonedInputData, { [key]: setter(value) });
+
+	// FIXME: what if path is not exists in base data?
+	const value = getProperty(clonedInputData, key.toString());
+
+	// @ts-expect-error not planned to support non-exists path
+	const outputData = setProperty(clonedInputData, key.toString(), setter(value));
 	return outputData;
 }
 
