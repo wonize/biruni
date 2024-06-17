c8d0dbfa0dfa2e9ff2c5cd277b896df6075538c3
mikoloism <mikoloism@gmail.com>
2024-06-17 17:08:35 +0330

    fix(pkg/core): cover edge cases of `set/by-setter`
    fix type return of `setter` callback with `DeepPartial` utility type
    add type guard (if statements)
    replace `lodash.clonedeep` with `helpers/clone`
    add `lodash.merge` supportation

--- packages/core/src/set/by-setter.ts
+++ packages/core/src/set/by-setter.ts
@@ -1,23 +1,50 @@
-import { clone, type StoreData } from '../helpers/mod';
+import merge from 'lodash.merge';
+import clone from 'lodash.clone';
+import { deepKeys, hasProperty } from 'dot-prop';
+import type { DeepPartial } from '../helpers/deep-partial';
+import type { StoreData } from '../helpers/mod';
 
 interface SetBySetter<Data extends StoreData> {
 	<Setter extends SetBySetterFunction<Data>>(setter: Setter): Promise<void>;
 }
 
 interface SetBySetterFunction<Data extends StoreData> {
-	(data: Readonly<Data>): Partial<Data>;
+	(data: Readonly<Data>): DeepPartial<Data>;
 }
+
 
 const isBySetter = <Data extends StoreData>(input: unknown): input is SetBySetterFunction<Data> => {
 	return typeof input === 'function';
 };
 
-function setBySetter<Data extends StoreData, Setter extends SetBySetterFunction<Data>>(
+function setBySetter<Data extends StoreData, Setter extends SetBySetterFunction<Data> = SetBySetterFunction<Data>>(
 	data: Data,
 	setter: Setter
 ): Data {
+	if (typeof data !== 'object' || typeof data === 'function') {
+		throw 'Error';
+	}
+
+
 	const clonedInputData = clone(data);
-	const outputData = Object.assign({}, clonedInputData, setter(clonedInputData));
+	const value = setter(clonedInputData);
+
+	if (typeof value !== 'object' || typeof value === 'function') {
+		throw 'Error';
+	}
+
+	let is_match: boolean = true;
+	for (const path of deepKeys(value)) {
+		if (hasProperty(clonedInputData, path) === false) {
+			is_match = false;
+		}
+	}
+
+	if (is_match === false) {
+		return clonedInputData;
+	}
+
+	const outputData = merge(clonedInputData, value);
 	return outputData;
 }
 
