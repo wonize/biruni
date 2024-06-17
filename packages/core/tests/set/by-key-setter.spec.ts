a744b86c8987fb570320b1d89f58d665dc339f51
mikoloism <mikoloism@gmail.com>
2024-06-17 15:56:27 +0330

    test(pkg/core): add `set/by-key-setter` tests
    cover usage and edge case

---
+++ packages/core/tests/set/by-key-setter.spec.ts
@@ -0,0 +1,140 @@
+import { mockData, type MockData } from '@repo/mocks';
+import { describe, expect, expectTypeOf, it, vi } from 'vitest';
+
+import type { Path } from '@/path/mod';
+import {
+	isByKeySetter,
+	type SetByKeySetterFunction,
+	setByKeySetter,
+	type SetByKeySetter,
+} from '@/set/by-key-setter.ts';
+import { isKeyOfData } from '@/set/by-key-value.ts';
+
+describe('set/by-key-setter.ts', () => {
+	describe('Verify Signature', () => {
+		it('Verify the Function Signature and Return Type of the <isByKeySetter>', () => {
+			expectTypeOf(isByKeySetter).toBeFunction();
+			expectTypeOf(isByKeySetter).parameter(0).toBeUnknown();
+			expectTypeOf(isByKeySetter).returns.toBeBoolean();
+		});
+
+		it('Verify the Function Signature and Return Type of the <isKeyOfData>', () => {
+			expectTypeOf(isKeyOfData).toBeFunction();
+			expectTypeOf(isKeyOfData).parameter(0).toBeUnknown();
+			expectTypeOf(isKeyOfData).returns.toBeBoolean();
+		});
+
+		it('Verify the Type Signature of the <setByKeySetter> Helper Function', () => {
+			expectTypeOf(setByKeySetter<MockData>).toBeFunction();
+			expectTypeOf(setByKeySetter<MockData>)
+				.parameter(0)
+				.toEqualTypeOf<MockData>();
+			expectTypeOf(setByKeySetter<MockData>)
+				.parameter(1)
+				.toEqualTypeOf<Path.From<MockData>>();
+			expectTypeOf(setByKeySetter<MockData>)
+				.parameter(2)
+				.toEqualTypeOf<SetByKeySetterFunction<Path.At<MockData, Path.From<MockData>>, MockData>>();
+			expectTypeOf(setByKeySetter<MockData>).returns.toEqualTypeOf<MockData>();
+		});
+
+		it('Verify the Type Signature of the <SetByKeySetter> Method Interface', () => {
+			expectTypeOf<SetByKeySetter<MockData>>().toBeFunction();
+			expectTypeOf<SetByKeySetter<MockData>>()
+				.parameter(0)
+				.toEqualTypeOf<Path.From<MockData>>();
+			expectTypeOf<SetByKeySetter<MockData>>()
+				.parameter(1)
+				.toEqualTypeOf<SetByKeySetterFunction<Path.At<MockData, Path.From<MockData>>, MockData>>();
+			expectTypeOf<SetByKeySetter<MockData>>().returns.toEqualTypeOf<Promise<void>>();
+		});
+	});
+
+	describe('Test Functionality', () => {
+		it('should call the setter with the given key once', () => {
+			const setter = vi.fn((_lang: MockData['lang']): MockData['lang'] => 'FR');
+			const expected = { ...mockData, lang: 'FR' };
+			const result = setByKeySetter(mockData, 'lang', setter);
+			expect(setter).toBeCalledTimes(1);
+			expect(setter).toBeCalledWith('EN');
+			expect(setter).toReturnWith('FR');
+			expect(result).toMatchObject(expected);
+			expect(result).not.toMatchObject(mockData);
+			expect(expected).not.toMatchObject(mockData);
+		});
+
+		it('should call the setter with the given nested key once and set the nested value', () => {
+			const setter = vi.fn((_currency) => ({ amount: 5000 }));
+			const expected = { ...mockData, currency: { ...mockData['currency'], amount: 5000 } };
+			const result = setByKeySetter(mockData, 'currency', setter);
+			expect(setter).toBeCalledTimes(1);
+			expect(setter).toBeCalledWith(expect.objectContaining({ amount: 1000, code: 'USD' }));
+			expect(setter).toReturnWith(expect.objectContaining({ amount: 5000 }));
+			expect(result).toMatchObject(expected);
+			expect(result).not.toMatchObject(mockData);
+			expect(expected).not.toMatchObject(mockData);
+		});
+
+		it('should call the setter with the given path key once and set the path value', () => {
+			const setter = vi.fn((_currency_amount) => 5000);
+			const expected = { ...mockData, currency: { ...mockData['currency'], amount: 5000 } };
+			const result = setByKeySetter(mockData, 'currency.amount', setter);
+			expect(setter).toBeCalledTimes(1);
+			expect(setter).toBeCalledWith(1000);
+			expect(setter).toReturnWith(5000);
+			expect(result).toMatchObject(expected);
+			expect(result).not.toMatchObject(mockData);
+			expect(expected).not.toMatchObject(mockData);
+		});
+
+		it('should return original object if the setter function does not return value', () => {
+			const setter = vi.fn((_lang) => { });
+			const expected = { ...mockData };
+			// @ts-expect-error to test empty setter
+			const result = setByKeySetter(mockData, 'lang', setter);
+			expect(setter).toBeCalledTimes(1);
+			expect(setter).toBeCalledWith('EN'); expect(setter).toReturnWith(undefined);
+			expect(result).toMatchObject(expected);
+			expect(result, 'no same reference').not.toBe(mockData);
+			expect(expected, 'no same reference').not.toBe(mockData);
+		});
+
+		it('should not call the setter function when the key does not exist', () => {
+			const setter = vi.fn((_nonexists) => 'value');
+			const expected = { ...mockData };
+			// @ts-expect-error to test non-exists key
+			const result = setByKeySetter(mockData, 'nonexists', setter);
+			expect(setter).toBeCalledTimes(0);
+			expect(result).toMatchObject(expected);
+		});
+
+		it('should throw an error when called with a non-object base', () => {
+			const setter = vi.fn(() => { });
+			// @ts-expect-error to test non-object
+			expect(() => setByKeySetter('non-object', 'lang', setter)).toThrowError();
+			expect(setter).toBeCalledTimes(0);
+		});
+
+		it('should throw an error when called with a non-string key', () => {
+			const setter = vi.fn(() => { });
+
+			// @ts-expect-error to test non-string key
+			expect(() => setByKeySetter(mockData, {}, setter)).toThrowError();
+			// @ts-expect-error to test non-string key
+			expect(() => setByKeySetter(mockData, undefined, setter)).toThrowError();
+		});
+
+		it.todo('should return resolved value if setter function is a Promise', () => {
+			const setter = vi.fn(async (_lang: MockData['lang']): Promise<MockData['lang']> => 'FR');
+			const expected = { ...mockData, lang: 'FR' };
+			// @ts-expect-error to test Promise-like setter
+			const result = setByKeySetter(mockData, 'lang', setter);
+			expect(setter).toBeCalledTimes(1);
+			expect(setter).toBeCalledWith(mockData['lang']);
+			expect(setter).toReturnWith(expected['lang']);
+			expect(result).toMatchObject(expected);
+			expect(result).not.toMatchObject(mockData);
+			expect(expected).not.toMatchObject(mockData);
+		});
+	});
+});
