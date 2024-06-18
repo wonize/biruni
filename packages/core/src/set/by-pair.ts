8bc2f8f3760a3444416a0b8a80f976f3486a847f
mikoloism <mikoloism@gmail.com>
2024-06-18 16:35:23 +0330

    feat(pkg/core): fit `set/by-pair` with itself test cases
    add type-guards
    add edge-cases coverage and handle eexceptions
    use `lodash.clonedeep` and `lodash.merge`
    use `dot-prop/deleteProperty`, `dot-prop/hasProperty` and `dot-prop/deepKeys`
    use `DeepPartial` utility type

--- packages/core/src/set/by-pair.ts
+++ packages/core/src/set/by-pair.ts
@@ -1,18 +1,44 @@
-import { clone, type StoreData } from '../helpers/mod';
+import { deepKeys, deleteProperty, hasProperty } from 'dot-prop';
+import clone from 'lodash.clonedeep';
+import merge from 'lodash.merge';
+import type { StoreData } from '../helpers/mod';
+import type { DeepPartial } from '../helpers/deep-partial';
 
 interface SetByPair<Data extends StoreData> {
-	(data: Partial<Data>): Promise<void>;
+	(data: DeepPartial<Data>): Promise<void>;
 }
 
-const isByPair = <Data extends StoreData>(input: unknown): input is Partial<Data> => {
+const isByPair = <Data extends StoreData>(input: unknown): input is Partial<Data> | DeepPartial<Data> => {
 	return typeof input === 'object' && input !== null;
 };
 
-function setByPair<Data extends StoreData>(data: Data, pairs: Partial<Data>): Data {
-	const cloneInputData = clone(data);
-	const clonedPair = clone(pairs);
-	const outputData = Object.assign({}, cloneInputData, clonedPair);
-	return outputData;
+function setByPair<Data extends StoreData>(data: Data, pairs: DeepPartial<Data>): Data {
+	if ((typeof pairs !== 'object' || pairs === null) && (typeof data !== 'object' || data === null)) {
+		return Object.create({});
+	}
+
+	if (typeof pairs !== 'object' || pairs === null) {
+		return clone(data);
+	}
+
+	if (typeof data !== 'object' || data === null) {
+		return clone(pairs as Data);
+	}
+
+	const cloned_base = clone(data);
+	const cloned_pair = clone(pairs);
+
+	if (JSON.stringify(cloned_base).length === 2) {
+		return cloned_pair as Data;
+	}
+
+	for (const key of deepKeys(cloned_pair)) {
+		if (hasProperty(cloned_base, key) === false) {
+			deleteProperty(cloned_pair, key);
+		}
+	}
+
+	return merge(cloned_base, cloned_pair);
 }
 
 export { isByPair, setByPair };
