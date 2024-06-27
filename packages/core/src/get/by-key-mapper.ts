7662f2279db4d79b8177f92141cc9b27839fdc62
mikoloism <mikoloism@gmail.com>
2024-06-27 21:38:36 +0330

    feat(pkg/core): support path keys
    improve`by-key-mapper` utility
    sync with test cases
    use `lodash.clonedeep` instead of custom clone

--- packages/core/src/get/by-key-mapper.ts
+++ packages/core/src/get/by-key-mapper.ts
@@ -1,7 +1,10 @@
 import type { StoreData } from '../helpers/mod';
+import clone from 'lodash.clonedeep';
+import { getProperty, hasProperty } from 'dot-prop';
+import type { Path } from '../path/mod';
 
 interface GetByKeyMapper<Data extends StoreData> {
-	<Key extends keyof Data, Mapper extends GetByKeyMapperFunction<Data, Key>>(
+	<Key extends Path.From<Data>, Mapper extends GetByKeyMapperFunction<Data, Key>>(
 		key: Key,
 		mapper: Mapper
 	): Promise<GetByKeyMapperReturnType<Data, Key, Mapper>>;
@@ -9,26 +12,39 @@ interface GetByKeyMapper<Data extends StoreData> {
 
 type GetByKeyMapperReturnType<
 	Data extends StoreData,
-	Key extends keyof Data,
+	Key extends Path.From<Data>,
 	Mapper extends GetByKeyMapperFunction<Data, Key>,
-> = ReturnType<Mapper> | Data[Key] | unknown;
+> = ReturnType<Mapper> | Path.At<Data, Key> | unknown;
 
-interface GetByKeyMapperFunction<Data extends StoreData, Key extends keyof Data> {
-	(data: Data[Key] | never): Data[Key] | unknown;
+interface GetByKeyMapperFunction<Data extends StoreData, Key extends Path.From<Data>> {
+	(data: Path.At<Data, Key> | never): Path.At<Data, Key> | unknown;
 }
 
 const isByKeyMapper = <Data extends StoreData>(
 	input: unknown
-): input is GetByKeyMapperFunction<Data, keyof Data> => {
+): input is GetByKeyMapperFunction<Data, Path.From<Data>> => {
 	return typeof input === 'function';
 };
 
 function getByKeyMapper<
 	Data extends StoreData,
-	Key extends keyof Data,
-	Mapper extends GetByKeyMapperFunction<Data, Key>,
+	Key extends Path.From<Data> = Path.From<Data>,
+	Mapper extends GetByKeyMapperFunction<Data, Key> = GetByKeyMapperFunction<Data, Key>,
 >(data: Data, key: Key, mapper: Mapper) {
-	return mapper(data[key]);
+	let temp_mapper = mapper;
+
+	if (typeof mapper !== 'function') {
+		temp_mapper = function alternative_mapper(value) {
+			return value;
+		} as Mapper
+	}
+
+	if (hasProperty(data, key.toString()) === false) {
+		return clone(data);
+	}
+
+	const parameter = getProperty(data, key.toString())!;
+	return temp_mapper(parameter);
 }
 
 export { getByKeyMapper, isByKeyMapper };
