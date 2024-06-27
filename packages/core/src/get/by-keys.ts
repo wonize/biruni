abca4fb6d44da94ca4439926c361d1aeb00ebb22
mikoloism <mikoloism@gmail.com>
2024-06-27 21:40:02 +0330

    feat(pkg/core): support path keys
    improve`by-keys` utility
    refactor functionality and behavior
    cover the test cases
    use `lodash.clonedeep` instead of custom clone

--- packages/core/src/get/by-keys.ts
+++ packages/core/src/get/by-keys.ts
@@ -1,19 +1,26 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { getProperty } from 'dot-prop';
+import merge from 'lodash.merge';
+import cloneDeep from 'lodash.clonedeep';
+import type { Path } from '../path/mod';
 import type { StoreData } from '../helpers/mod';
 
 interface GetByKeys<Data extends StoreData> {
-	<KeyList extends Array<keyof Data>>(
+	<KeyList extends Array<Path.From<Data>>>(
 		keys: Partial<KeyList>
 	): Promise<GetByKeysReturnType<Data, KeyList>>;
 }
 
 type GetByKeysReturnType<
 	Data extends StoreData,
-	KeyList extends Partial<Array<keyof Data>>,
+	KeyList extends Partial<Array<Path.From<Data>>>,
 > = Readonly<{
 	[SelectedKey in KeyList extends Partial<Array<infer Key>> ? Key : never]: Data[SelectedKey];
 }>;
 
-const isByKeys = <Data extends StoreData>(input: unknown): input is Partial<Array<keyof Data>> => {
+const isByKeys = <Data extends StoreData>(
+	input: unknown
+): input is Partial<Array<Path.From<Data>>> => {
 	return (
 		typeof input === 'object' &&
 		input !== null &&
@@ -21,15 +28,36 @@ const isByKeys = <Data extends StoreData>(input: unknown): input is Partial<Arra
 	);
 };
 
-function getByKeys<Data extends StoreData, KeyList extends Array<keyof Data>>(
-	data: Data,
-	keys: Partial<KeyList>
-) {
-	const filtered_data = (keys as unknown as Array<keyof Data>).reduce((filtered_pairs, key) => {
-		return Object.assign({}, filtered_pairs, { [key]: data[key] });
-	}, {});
+function getByKeys<
+	Data extends StoreData,
+	KeyList extends Array<Path.From<Data>> = Array<Path.From<Data>>,
+>(data: Data, keys: Partial<KeyList>) {
+	let temp_base = data;
+	if (typeof data !== 'object' || data === null) {
+		temp_base = Object.create({});
+	}
+
+	const cloned_base = cloneDeep(temp_base);
+
+	let result = Object.create({});
+	for (const key of keys) {
+		const value = getProperty(cloned_base, key!.toString());
+		if (key?.toString().indexOf('.') !== -1) {
+			const parts = key!.toString().split('.');
+			for (const part of parts.slice(1)) {
+				result = merge(result, {
+					[parts[0]!.toString()]: {
+						[part!.toString()]: value,
+					},
+				});
+			}
+		} else {
+			result = merge(result, { [key!.toString()]: value });
+		}
+	}
+
 	type Result = GetByKeysReturnType<Data, typeof keys>;
-	return filtered_data as unknown as Result;
+	return result as unknown as Result;
 }
 
 export { getByKeys, isByKeys };
