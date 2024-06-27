2dcd0f6a63c9afd7e751bb37fe604d7fdecfebb1
mikoloism <mikoloism@gmail.com>
2024-06-27 21:39:02 +0330

    feat(pkg/core): support path keys
    improve`by-key-value` utility
    sync with test cases
    use `lodash.clonedeep` instead of custom clone

--- packages/core/src/get/by-key-value.ts
+++ packages/core/src/get/by-key-value.ts
@@ -1,12 +1,14 @@
+import { getProperty } from 'dot-prop';
+import type { Path } from '../path/mod';
 import type { StoreData } from '../helpers/mod';
 
 interface GetByKey<Data extends StoreData> {
-	<Key extends keyof Data>(key: Key): Promise<GetByKeyReturnType<Data, Key>>;
+	<Key extends Path.From<Data>>(key: Key): Promise<GetByKeyReturnType<Data, Key>>;
 }
 
-type GetByKeyReturnType<Data extends StoreData, Key extends keyof Data> = Data[Key];
+type GetByKeyReturnType<Data extends StoreData, Key extends Path.From<Data>> = Path.At<Data, Key>;
 
-const isKeyOfData = <Data extends StoreData>(input: unknown): input is keyof Data => {
+const isKeyOfData = <Data extends StoreData>(input: unknown): input is Path.From<Data> => {
 	return typeof input === 'string';
 };
 
@@ -16,8 +18,15 @@ const isByKey = <Data extends StoreData>(
 	return typeof input === 'undefined' || input === null || Boolean(input) === false;
 };
 
-function getByKey<Data extends StoreData, Key extends keyof Data>(data: Data, key: Key) {
-	return data[key];
+function getByKey<Data extends StoreData, Key extends Path.From<Data> = Path.From<Data>>(
+	data: Data,
+	key: Key
+): Path.At<Data, Key> | undefined {
+	let temp_base = data;
+	if (typeof data !== 'object' || data === null) {
+		temp_base = Object.create({});
+	}
+	return getProperty(temp_base, key.toString());
 }
 
 export { getByKey, isByKey, isKeyOfData };
