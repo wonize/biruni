d592ce00467cbc0e90df333437759ab47a304de2
mikoloism <mikoloism@gmail.com>
2024-06-28 17:30:51 +0330

    test(pkg/core): cover `methods` and `mod` from `get` scope

---
+++ packages/core/tests/get/methods.spec.ts
@@ -0,0 +1,62 @@
+import { expectTypeOf, describe, it } from 'vitest';
+import type { MockData } from '@repo/mocks';
+import type { GetInterface } from '@/get/methods';
+import type { GetOverloads } from '@/get/overloads';
+import type { GetByEntire } from '@/get/by-entire';
+import type { GetByKey } from '@/get/by-key-value';
+import type { GetByKeyMapper } from '@/get/by-key-mapper';
+import type { GetByMapper } from '@/get/by-mapper';
+import type { GetByKeys } from '@/get/by-keys';
+import type { GetByTruthy } from '@/get/by-truthy';
+
+describe('get/methods.ts', () => {
+	it('should have 5 method related started with "get" keyword', () => {
+		expectTypeOf<GetInterface<MockData>>().toHaveProperty('get');
+		expectTypeOf<GetInterface<MockData>>().toHaveProperty('getByEntire');
+		expectTypeOf<GetInterface<MockData>>().toHaveProperty('getByKey');
+		expectTypeOf<GetInterface<MockData>>().toHaveProperty('getByKeyMapper');
+		expectTypeOf<GetInterface<MockData>>().toHaveProperty('getByMapper');
+	});
+
+	it('should match <get> overloaded signature method', () => {
+		expectTypeOf<GetInterface<MockData>>().toHaveProperty('get');
+		expectTypeOf<GetInterface<MockData>['get']>().toBeFunction();
+		expectTypeOf<GetInterface<MockData>['get']>().toEqualTypeOf<GetOverloads<MockData>>();
+	});
+
+	it('should match <getByEntire> signature method', () => {
+		expectTypeOf<GetInterface<MockData>>().toHaveProperty('getByEntire');
+		expectTypeOf<GetInterface<MockData>['getByEntire']>().toBeFunction();
+		expectTypeOf<GetInterface<MockData>['getByEntire']>().toEqualTypeOf<GetByEntire<MockData>>();
+	});
+
+	it('should match <getByKeyValue> signature method', () => {
+		expectTypeOf<GetInterface<MockData>>().toHaveProperty('getByKey');
+		expectTypeOf<GetInterface<MockData>['getByKey']>().toBeFunction();
+		expectTypeOf<GetInterface<MockData>['getByKey']>().toEqualTypeOf<GetByKey<MockData>>();
+	});
+
+	it('should match <getByKeyMapper> signature method', () => {
+		expectTypeOf<GetInterface<MockData>>().toHaveProperty('getByKeyMapper');
+		expectTypeOf<GetInterface<MockData>['getByKeyMapper']>().toBeFunction();
+		expectTypeOf<GetInterface<MockData>['getByKeyMapper']>().toEqualTypeOf<GetByKeyMapper<MockData>>();
+	});
+
+	it('should match <getByMapper> signature method', () => {
+		expectTypeOf<GetInterface<MockData>>().toHaveProperty('getByMapper');
+		expectTypeOf<GetInterface<MockData>['getByMapper']>().toBeFunction();
+		expectTypeOf<GetInterface<MockData>['getByMapper']>().toEqualTypeOf<GetByMapper<MockData>>();
+	});
+
+	it('should match <getByKeys> signature method', () => {
+		expectTypeOf<GetInterface<MockData>>().toHaveProperty('getByKeys');
+		expectTypeOf<GetInterface<MockData>['getByKeys']>().toBeFunction();
+		expectTypeOf<GetInterface<MockData>['getByKeys']>().toEqualTypeOf<GetByKeys<MockData>>();
+	});
+
+	it('should match <getByTruthy> signature method', () => {
+		expectTypeOf<GetInterface<MockData>>().toHaveProperty('getByTruthy');
+		expectTypeOf<GetInterface<MockData>['getByTruthy']>().toBeFunction();
+		expectTypeOf<GetInterface<MockData>['getByTruthy']>().toEqualTypeOf<GetByTruthy<MockData>>();
+	});
+});
---
+++ packages/core/tests/get/mod.spec.ts
@@ -0,0 +1,33 @@
+import * as getBarrel from '@/get/mod.ts';
+
+describe('get/mod.ts', () => {
+	it('should re-export <getByEntire> utility', () => {
+		expect(getBarrel).toHaveProperty('getByEntire');
+		expectTypeOf(getBarrel['getByEntire']).toBeFunction();
+	})
+
+	it('should re-export <getByKey> utility', () => {
+		expect(getBarrel).toHaveProperty('getByKey');
+		expectTypeOf(getBarrel['getByKey']).toBeFunction();
+	})
+
+	it('should re-export <getByKeyMapper> utility', () => {
+		expect(getBarrel).toHaveProperty('getByKeyMapper');
+		expectTypeOf(getBarrel['getByKeyMapper']).toBeFunction();
+	})
+
+	it('should re-export <getByMapper> utility', () => {
+		expect(getBarrel).toHaveProperty('getByMapper');
+		expectTypeOf(getBarrel['getByMapper']).toBeFunction();
+	})
+
+	it('should re-export <getByKeys> utility', () => {
+		expect(getBarrel).toHaveProperty('getByKeys');
+		expectTypeOf(getBarrel['getByKeys']).toBeFunction();
+	})
+
+	it('should re-export <getByTruthy> utility', () => {
+		expect(getBarrel).toHaveProperty('getByTruthy');
+		expectTypeOf(getBarrel['getByTruthy']).toBeFunction();
+	})
+})
