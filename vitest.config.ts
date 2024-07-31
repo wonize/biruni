fd24a66be19eacf0e1936ba5829bb60a7506a05a
mikoloism <mikoloism@gmail.com>
2024-07-31 12:05:22 +0330

    chore(vitest.config): improve `reporter`, `exclude` and `include`

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,32 +1,37 @@
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { configDefaults, defineConfig } from 'vitest/config';
 
+const include = configDefaults.include
+	.concat('packages/core')
+	.concat('packages/factory')
+	.concat('packages/built-in')
+	.concat('packages/biruni');
+
+const exclude = configDefaults.exclude
+	.concat('_ignored_')
+	.concat('**/dist/**')
+	.concat('**/build/**')
+	.concat('**/scripts/**')
+	.concat('**/docs/**')
+	.concat('**/examples/**')
+	.concat('**/node_modules/**');
+
+const reporter = ['html', 'json', 'text', 'clover'];
+
 export default defineConfig({
-  plugins: [tsconfigPaths()],
-  test: {
-    name: 'unit',
-    globals: true,
-    environment: 'jsdom',
-    mockReset: false,
-    exclude: configDefaults.exclude
-      .concat('_ignored_')
-      .concat('**/dist/**')
-      .concat('**/build/**')
-      .concat('**/scripts/**')
-      .concat('**/docs/**')
-      .concat('**/examples/**')
-      .concat('**/node_modules/**'),
-    coverage: {
-      "provider": "v8",
-      "reporter": ["html", "json", "text", "clover"],
-      exclude: configDefaults.exclude
-        .concat('_ignored_')
-        .concat('**/dist/**')
-        .concat('**/build/**')
-        .concat('**/scripts/**')
-        .concat('**/docs/**')
-        .concat('**/examples/**')
-        .concat('**/node_modules/**'),
-    }
-  },
+	plugins: [tsconfigPaths()],
+	test: {
+		name: 'unit',
+		globals: true,
+		environment: 'jsdom',
+		mockReset: false,
+		exclude: exclude,
+		reporters: reporter.slice(0, 1),
+		coverage: {
+			provider: 'v8',
+			reporter: reporter,
+			include: include,
+			exclude: exclude,
+		},
+	},
 });
