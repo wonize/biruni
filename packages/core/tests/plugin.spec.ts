1192a7defb238214eab027427105a0cc1587de2d
mikoloism <mikoloism@gmail.com>
2024-08-15 21:19:27 +0330

    test(core): cover concrete "Plugin" cases

--- packages/core/tests/plugin.spec.ts
+++ packages/core/tests/plugin.spec.ts
@@ -1,38 +1,56 @@
 import { Core } from '@/core';
 import { DataFlow } from '@/flow';
 import { Plugin } from '@/plugin';
-import type { MockData } from '@repo/mocks';
+import type { MOCK_NAMESPACE, MockData } from '@repo/mocks';
 
-describe('Plugin interface', () => {
-	const instance = expectTypeOf<Plugin<MockData>>();
+describe('core/plugin.ts', () => {
+	describe('Plugin Interface', () => {
+		const instance = expectTypeOf<Plugin<MockData>>();
 
-	class PluginImpl extends Plugin<MockData> {
-		public constructor() {
-			super('impl');
+		it('should to have <process> method', () => {
+			const process = instance.toHaveProperty('process');
+			process.toBeFunction();
+			process.parameter(0).toBeObject();
+			process.parameter(1).toBeUndefined();
+			process.returns.toBeObject();
+		});
+
+		it('should to have <setup> method', () => {
+			const setup = instance.toHaveProperty('setup');
+			setup.toBeFunction();
+			setup.parameter(0).toEqualTypeOf<Core<MockData>>();
+			setup.returns.toBeVoid();
+		});
+
+		it.todo('should to have <flow> attribute and default initialized', () => {
+			const flow = instance.toHaveProperty('flow');
+			flow.toEqualTypeOf<DataFlow>();
+		});
+	});
+
+	describe('Plugin Functionality', () => {
+		class ConcretePlugin extends Plugin<MockData> {
+			public constructor() {
+				super('concrete');
+			}
 		}
-	}
 
-	it('should to have <process> method', () => {
-		const process = instance.toHaveProperty('process');
-		process.toBeFunction();
-		process.parameter(0).toBeObject();
-		process.parameter(1).toBeUndefined();
-		process.returns.toBeObject();
-	});
+		const concrete = new ConcretePlugin();
 
-	it('should to have <setup> method', () => {
-		const setup = instance.toHaveProperty('setup');
-		setup.toBeFunction();
-		setup.parameter(0).toEqualTypeOf<Core<MockData>>();
-		setup.returns.toBeVoid();
-	});
+		const setupSpy = vi.spyOn(concrete, 'setup');
+		it('should call <setup> and assign the given parameter to <core>', () => {
+			concrete.setup({ __mock__: true } as unknown as Core<MockData, typeof MOCK_NAMESPACE>);
+			expect(setupSpy).toBeCalledTimes(1);
+			expect(setupSpy).toBeCalledWith(expect.objectContaining({ __mock__: true }));
+			expect(concrete['core']).toStrictEqual(expect.objectContaining({ __mock__: true }));
+		});
 
-	it('should to have <flow> attribute and default initialized', () => {
-		const flow = instance.toHaveProperty('flow');
-		flow.toEqualTypeOf<DataFlow>();
-		const plugin = new PluginImpl();
-		expect(plugin.flow).toStrictEqual(DataFlow.NONE);
-		expect(plugin.flow).not.toStrictEqual(DataFlow.INPUT);
-		expect(plugin.flow).not.toStrictEqual(DataFlow.OUTPUT);
+		const processSpy = vi.spyOn(concrete, 'process');
+		it('should call <process> and return the given parameter', () => {
+			concrete.process({ __mock__: true } as unknown as MockData);
+			expect(processSpy).toBeCalledTimes(1);
+			expect(processSpy).toBeCalledWith(expect.objectContaining({ __mock__: true }));
+			expect(processSpy).toReturnWith(expect.objectContaining({ __mock__: true }));
+		});
 	});
 });
