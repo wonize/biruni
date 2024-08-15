6a4f5998dfcae892f563f0308a26854f4a0780b8
mikoloism <mikoloism@gmail.com>
2024-08-15 18:36:05 +0330

    feat(core)!: support property accessors
    each property in "Data" will be have two functionality.
    with or without passed parameters.
    with parameters, will do "set" job.
    without paramters, will do "get" job.
    
    NOTE: is under beta stage
    
    BREAKING CHANGE: support to propertizer the accessors of boundaries

--- packages/core/src/accessor.ts
+++ packages/core/src/accessor.ts
@@ -114,4 +114,40 @@ class HasAccessor<Data extends StoreData> extends Plugin<Data> {
 	};
 }
 
-export { GetAccessor, HasAccessor, SetAccessor };
+class PropertyAccessor<Data extends StoreData> extends Plugin<Data> {
+	public constructor() {
+		super('accessor.property');
+	}
+
+	public override setup(core: Core<Data, string>): void {
+		super.setup(core);
+		this.produce_boundaries(core);
+	}
+
+	private produce_boundaries(core: Core<Data>): void {
+		for (const key in (core as unknown as { data: Data }).data) {
+			this.produce_boundary.call(core, key);
+		}
+	}
+
+	private produce_boundary<Key extends keyof Data>(this: Core<Data>, key: Key) {
+		type Value = Data[Key];
+
+		const boundary = function (this: Core<Data>) {
+			function boundary(this: Core<Data>, value: Value | null = null) {
+				if (value === null) {
+					this.process(DataFlow.INPUT, (data) => {
+						Object.assign(data, { [key]: value });
+						return data;
+					});
+				}
+				return this.process(DataFlow.OUTPUT, (data) => data[key]);
+			}
+
+			return boundary.bind(this);
+		}.bind(this)();
+		this.bound(key as string, boundary);
+	}
+}
+
+export { GetAccessor, HasAccessor, SetAccessor, PropertyAccessor };
