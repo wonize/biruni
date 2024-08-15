4e5fbf8b5cd09fc9c93a53228c6333f890651412
mikoloism <mikoloism@gmail.com>
2024-08-15 20:35:00 +0330

    refactor(core): remove unused "plugin" directory

--- packages/core/src/plugin/mod.ts
+++
@@ -1,6 +0,0 @@
-export { BiruniPlugin } from './function';
-export { postprocess, preprocess } from './process';
-
-export type { ContextType } from './context';
-export type { PluginFunction as Function, PluginInterface } from './function';
-export type { PluginStack as Stack } from './stack';
--- packages/core/src/plugin/stack.ts
+++
@@ -1,4 +0,0 @@
-import type { StoreData } from '../helpers/mod';
-import type { BiruniPlugin } from './function';
-
-export type PluginStack<Data extends StoreData> = Array<BiruniPlugin<Data>>;
