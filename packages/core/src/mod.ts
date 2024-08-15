7181dc1ce90f40f0ffbece3891ec68eadb2ee233
mikoloism <mikoloism@gmail.com>
2024-08-15 20:27:25 +0330

    refactor(core): export new "Core" module
    remove previous "Store" modules
    remove unused "get-sync.ts" file

--- packages/core/src/mod.ts
+++ packages/core/src/mod.ts
@@ -1,10 +1,3 @@
-export type * as Context from './plugin/context';
-export type * as Plugin from './plugin/mod';
-
-export type * as GetterSync from './get-sync';
-export * as Getter from './get/mod';
-export type * as Event from './listener/event';
-export type * as Listener from './listener/mod';
-export * as Setter from './set/mod';
-export { Store, Store as default } from './store';
-export type { StoreInterface } from './store';
+export { Core, Core as default } from './core';
+export { DataFlow } from './flow';
+export { Plugin } from './plugin';
