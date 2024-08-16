b845bd5b44a3c36c337e3188917b6e525f5f64c3
mikoloism <mikoloism@gmail.com>
2024-08-16 21:09:15 +0330

    test(core): write termporaly cases for "core" class

--- packages/core/tests/core.spec.ts
+++ packages/core/tests/core.spec.ts
@@ -1,7 +1,7 @@
 import { Core } from '@/core';
 import { DataFlow } from '@/flow';
 import { Plugin } from '@/plugin';
-import type { MockData } from '@repo/mocks';
+import { MOCK_NAMESPACE, mockData, type MockData } from '@repo/mocks';
 
 describe('core/core.ts', () => {
 	describe('Interface', () => {
@@ -16,8 +16,8 @@ describe('core/core.ts', () => {
 			bound.parameter(1).toBeUnknown();
 		});
 
-		it.skip('should to have <namespace> accessor', () => {
-			prototype.toHaveProperty('namespace');
+		it('should to have <namespace> accessor', () => {
+			instance.toHaveProperty('namespace');
 		});
 
 		it('should to have <plug> method', () => {
@@ -75,10 +75,160 @@ describe('core/core.ts', () => {
 		});
 	});
 
-	describe.todo('Functionality <Core>', () => {
-		// TODO: write plugin tests and make mocks of plugins
-		// put all together in here
+	describe('Functionality <Core>', () => {
+		const core = new Core(MOCK_NAMESPACE);
+		const processSpy = vi.spyOn(core, 'process');
+		const plugSpy = vi.spyOn(core, 'plug');
+		const registerSpy = vi.spyOn(core, 'register');
+		const invokeSpy = vi.spyOn(core, 'invoke');
+		const triggerSpy = vi.spyOn(core, 'trigger');
+		const watchSpy = vi.spyOn(core, 'watch');
+		const boundSpy = vi.spyOn(core, 'bound');
+		const initSpy = vi.spyOn(core, 'init');
 
-		const core = new Core('namespace');
+		it('should to register new internal function and invoke correctly', () => {
+			const internal = vi.fn().mockImplementation((param) => ({ result: param.param }));
+			core.register('custom', internal);
+
+			expect(Array.from(core['internal'].entries())).toStrictEqual(
+				expect.arrayContaining([expect.arrayContaining(['custom', internal])])
+			);
+
+			expect(registerSpy).toBeCalledTimes(1);
+			expect(registerSpy).toBeCalledWith('custom', internal);
+			expect(registerSpy).toReturnWith(expect.objectContaining({ custom: internal }));
+
+			core.invoke('custom', { param: 'value' });
+			expect(invokeSpy).toBeCalledTimes(1);
+			expect(invokeSpy).toBeCalledWith('custom', expect.objectContaining({ param: 'value' }));
+			expect(invokeSpy).toReturnWith(expect.objectContaining({ result: 'value' }));
+
+			expect(internal).toBeCalledTimes(1);
+			expect(internal).toBeCalledWith(expect.objectContaining({ param: 'value' }));
+			expect(internal).toReturnWith(expect.objectContaining({ result: 'value' }));
+		});
+
+		it('should throw excepction when internal not exists', () => {
+			expect(() => {
+				core.invoke('bluh', 'value');
+			}).toThrowError();
+		});
+
+		it('should register a <hook> listener', () => {
+			const fn = vi.fn().mockImplementation((p) => ({ result: p }));
+			core.watch('onCustom', fn);
+
+			expect(Array.from(core['hooks'])).toStrictEqual(
+				expect.arrayContaining([expect.arrayContaining(['onCustom', new Set([fn])])])
+			);
+
+			expect(watchSpy).toBeCalledTimes(1);
+			expect(watchSpy).toBeCalledWith('onCustom', fn);
+			expect(watchSpy).toReturnWith(expect.objectContaining({ onCustom: fn }));
+
+			expect(fn).toBeCalledTimes(0);
+
+			core.trigger('onCustom', 'value');
+
+			expect(fn).toBeCalledTimes(1);
+			expect(fn).toBeCalledWith('value');
+			expect(fn).toReturnWith(expect.objectContaining({ result: 'value' }));
+
+			expect(triggerSpy).toBeCalledTimes(1);
+			expect(triggerSpy).toBeCalledWith('onCustom', 'value');
+			expect(triggerSpy).toReturnWith(undefined);
+		});
+
+		it('should register a boundary', () => {
+			const fn = vi.fn().mockImplementation((p) => ({ result: p }));
+			core.bound('fn', fn);
+			expect(boundSpy).toBeCalledWith('fn', fn);
+
+			core.bound('value', 'value');
+			expect(boundSpy).toBeCalledWith('value', 'value');
+
+			expect(boundSpy).toBeCalledTimes(2);
+		});
+
+		it('should register a plugin', () => {
+			const mockPlugin = { setup: vi.fn() } as unknown as Plugin<MockData>;
+
+			core.plug(mockPlugin);
+
+			expect(mockPlugin.setup).toReturnTimes(1);
+			expect(mockPlugin.setup).toBeCalledWith(core);
+		});
+
+		it('should pipe with INPUT', () => {
+			const mockPlugin = {
+				flow: DataFlow.INPUT,
+				setup: vi.fn(),
+				process: vi.fn().mockImplementation((d) => d),
+			} as unknown as Plugin<object>;
+			core.plug(mockPlugin);
+
+			const fn = vi.fn().mockImplementation(() => ({ value: 'value' }));
+			core.process(DataFlow.INPUT, fn);
+
+			expect(mockPlugin.process).toBeCalledTimes(1);
+			expect(mockPlugin.process).toBeCalledWith(expect.objectContaining({ value: 'value' }));
+			expect(mockPlugin.process).toReturnWith(expect.objectContaining({ value: 'value' }));
+			expect(plugSpy).toReturnWith(void 0);
+		});
+
+		it('should pipe with OUTPUT', () => {
+			const mockPlugin = {
+				flow: DataFlow.OUTPUT,
+				setup: vi.fn(),
+				process: vi.fn().mockImplementation((d) => d),
+			} as unknown as Plugin<object>;
+			core.plug(mockPlugin);
+
+			const fn = vi.fn().mockImplementation(() => ({ value: 'value' }));
+			core.process(DataFlow.OUTPUT, fn);
+			expect(mockPlugin.process).toBeCalledTimes(1);
+			expect(mockPlugin.process).toBeCalledWith(expect.objectContaining({ value: 'value' }));
+			expect(mockPlugin.process).toReturnWith(expect.objectContaining({ value: 'value' }));
+		});
+
+		it('should return only <Data> on <NONE> flow', () => {
+			const mockPlugin = {
+				flow: DataFlow.NONE,
+				setup: vi.fn(),
+				process: vi.fn().mockImplementation((d) => d),
+			} as unknown as Plugin<object>;
+			core.plug(mockPlugin);
+
+			const fn = vi.fn().mockImplementation(() => ({ value: 'value' }));
+			core.process(DataFlow.OUTPUT, fn);
+			expect(mockPlugin.process).toBeCalledTimes(0);
+			expect(processSpy).toReturnWith(expect.objectContaining({ value: 'value' }));
+		});
+
+		it('should return only on <NONE> flow', () => {
+			const mockPlugin = {
+				flow: DataFlow.OUTPUT,
+				setup: vi.fn(),
+				process: vi.fn().mockImplementation((d) => d),
+			} as unknown as Plugin<object>;
+			core.plug(mockPlugin);
+
+			const fn = vi.fn().mockImplementation(() => ({ value: 'value' }));
+			core.process(DataFlow.NONE, fn);
+			expect(mockPlugin.process).toBeCalledTimes(0);
+			expect(processSpy).toReturnWith(expect.objectContaining({ value: 'value' }));
+		});
+
+		it('should accessor to namespace', () => {
+			expect(core.namespace).toStrictEqual(MOCK_NAMESPACE);
+		});
+
+		it('should assign <Data> to <set data> in <init>', () => {
+			const initializer = vi.fn().mockReturnValue(mockData);
+			core.init(initializer);
+			expect(initializer).toBeCalledTimes(1);
+			expect(initializer).toReturnTimes(1);
+			expect(initSpy).toReturnWith(expect.objectContaining({}));
+		});
 	});
 });
