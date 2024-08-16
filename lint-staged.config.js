6410e5c8366f7405b99381c2c3afdb70488e9c0b
mikoloism <mikoloism@gmail.com>
2024-08-16 19:32:27 +0330

    ci(lint-staged): reorder "format" first then "lint"

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 export default {
 	'*': [
-		// eslint (only staged files)
-		'node_modules/.bin/nr lint:fix:only',
 		// prettier (only staged files)
 		'node_modules/.bin/nr format:fix:only',
+		// eslint (only staged files)
+		'node_modules/.bin/nr lint:fix:only',
 	],
 };
