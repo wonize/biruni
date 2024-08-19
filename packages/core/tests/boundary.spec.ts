598082b6584078c24ee5605c58cdcfa9a416a4ff
mikoloism <mikoloism@gmail.com>
2024-08-19 18:24:21 +0330

    test(core): refactor "boundary.spec.ts"
    rename all "describe" blocks
    update all "it" blocks
    remaster again everything

--- packages/core/tests/boundary.spec.ts
+++ packages/core/tests/boundary.spec.ts
@@ -1,75 +1,77 @@
 import { Boundary } from '@/boundary';
 
-describe('core/boundary.ts', () => {
-	describe('Abstraction', () => {
-		type CustomBoundary = {
-			get: (key: string) => string;
-			has: (key: string) => boolean;
-			set: (key: string, value: string) => string;
-		};
+describe('Boundary class', () => {
+	type CustomBoundary = {
+		get: (key: string) => string;
+		has: (key: string) => boolean;
+		set: (key: string, value: string) => string;
+	};
+	const remove = vi.fn();
+	const initial_boundaries = { has: vi.fn(), get: vi.fn(), set: vi.fn() };
+	const boundary = new Boundary<CustomBoundary>(initial_boundaries);
 
-		const prototype = expect(Boundary.prototype);
-		const instance = expectTypeOf(Boundary<CustomBoundary>).instance;
+	describe('.inject', () => {
+		const inject = expectTypeOf(Boundary<CustomBoundary>).instance.toHaveProperty('inject');
 
-		it('should to have constructor with intial boundaries', () => {
-			expectTypeOf(Boundary).toBeConstructibleWith({
-				has: (key: string): boolean => !!key === true,
-			});
-			expectTypeOf(Boundary).constructorParameters.toEqualTypeOf<
-				[] | [boundaries?: Record<string, unknown>]
-			>();
+		it('should be a method of <Boundary> instance', () => {
+			expect(boundary).toHaveProperty('inject');
 		});
 
-		it('should to have <inject> method', () => {
-			prototype.toHaveProperty('inject');
-			const inject = instance.toHaveProperty('inject');
+		it('should be a function', () => {
 			inject.toBeFunction();
+		});
+
+		it('should accept a <string> as the first argument', () => {
 			inject.parameter(0).toBeString();
+		});
+
+		it('should accept a <unknown> as the second argument', () => {
 			inject.parameter(1).toBeUnknown();
+		});
+
+		it('should return an object shape', () => {
 			inject.returns.toBeObject();
 		});
 
-		it('should to have <eject> method', () => {
-			prototype.toHaveProperty('eject');
-			const eject = instance.toHaveProperty('eject');
-			eject.toBeFunction();
-			eject.parameter(0).toBeUndefined();
-			eject.returns.toBeObject();
-			eject.returns.toEqualTypeOf<CustomBoundary>();
+		it('should inject a function into the boundary', () => {
+			const result = boundary.inject('remove', remove);
+			expect(result).toHaveProperty('remove');
+			expect(result.remove).toBe(remove);
 		});
 	});
 
-	describe('Functionality', () => {
-		type CustomBoundary = {
-			get: (key: string) => string;
-			has: (key: string) => boolean;
-			set: (key: string, value: string) => string;
-		};
-		const has = vi.fn();
-		const get = vi.fn();
-		const set = vi.fn();
-		const delete_boundary = vi.fn();
-		const customBoundaries = { has, get, set };
-		const boundary = new Boundary<CustomBoundary>(customBoundaries);
+	describe('.eject', () => {
+		const eject = expectTypeOf(Boundary<CustomBoundary>).instance.toHaveProperty('eject');
 
-		const spy_inject = vi.spyOn(boundary, 'inject');
-		it('should inject object', () => {
-			boundary.inject('delete', delete_boundary);
-			expect(spy_inject).toBeCalledTimes(1);
-			expect(spy_inject).toBeCalledWith('delete', delete_boundary);
-			expect(spy_inject).toReturnWith(expect.objectContaining({ delete: delete_boundary }));
+		it('should be a method of <Boundary> instance', () => {
+			expect(boundary).toHaveProperty('eject');
 		});
 
-		const spy_eject = vi.spyOn(boundary, 'eject');
-		it('should eject object', () => {
-			const ejected = boundary.eject();
-			expect(spy_eject).toBeCalledTimes(1);
-			expect(spy_eject).toBeCalledWith();
-			expect(spy_eject).toReturnWith(expect.objectContaining({ delete: delete_boundary }));
-			expect(ejected).toHaveProperty('delete');
-			expect(ejected).toHaveProperty('has');
-			expect(ejected).toHaveProperty('get');
-			expect(ejected).toHaveProperty('set');
+		it('should be a function', () => {
+			eject.toBeFunction();
+		});
+
+		it('should accept no arguments', () => {
+			eject.parameters.toEqualTypeOf<[]>();
+		});
+
+		it('should return an object shape matching <Generic> argument', () => {
+			eject.returns.toBeObject();
+			eject.returns.toEqualTypeOf<CustomBoundary>();
+		});
+
+		it('should eject the stored boundaries correctly', () => {
+			const boundaries = boundary.eject();
+			expect(boundaries).toEqual(expect.objectContaining({ ...initial_boundaries, remove }));
+		});
+	});
+
+	describe('.constructor', () => {
+		it('should accept an optional initial <Record> of boundaries', () => {
+			type EmptyConstructorArgument = [];
+			type InitialConstructorArguments = [boundaries?: Record<string, unknown>];
+			type ConstructorArguments = EmptyConstructorArgument | InitialConstructorArguments;
+			expectTypeOf(Boundary).constructorParameters.toEqualTypeOf<ConstructorArguments>();
 		});
 	});
 });
