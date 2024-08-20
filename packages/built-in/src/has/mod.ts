cf9271269d492944ee5285e343c4bf4058ef302d
mikoloism <mikoloism@gmail.com>
2024-08-20 16:25:22 +0330

    refactor(built-in/has): barrel export correction

--- packages/built-in/src/has/mod.ts
+++ packages/built-in/src/has/mod.ts
@@ -1,21 +1,2 @@
-import type { DataObject } from '@biruni/core/helpers/type-utility';
-import { hasProperty } from 'dot-prop';
-import type { Path } from '../path/mod';
-
-function hasOwnPropertyPath<
-	Data extends DataObject,
-	PathKey extends Path.From<Data> = Path.From<Data>,
->(data: Data, path: PathKey): boolean {
-	if (typeof data !== 'object' || data === null) {
-		throw 'Error';
-	}
-
-	return hasProperty(data, path as unknown as string);
-}
-
-interface HasOwnPropertyPath<Data extends DataObject> {
-	<PathKey extends Path.From<Data>>(path: PathKey): boolean;
-}
-
-export { hasOwnPropertyPath };
-export type { HasOwnPropertyPath };
+export * from './has';
+export { HasAccessor } from './implement';
