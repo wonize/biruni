1b0f7ead178ca1f993dd99f607a0d5408fa4df65
mikoloism <mikoloism@gmail.com>
2024-08-22 15:27:48 +0330

    test(built-in/event): enhance test cases

--- packages/built-in/tests/event.spec.ts
+++ packages/built-in/tests/event.spec.ts
@@ -1,67 +1,91 @@
-import { event, EventEmitterPlugin } from '@/event/mod'
-import { diff } from '@biruni/core/listener/diff';
-import { BiruniPlugin } from '@biruni/core/plugin'
-import { mockData, type MockData } from '@repo/mocks'
-import { default as EventEmitter } from 'events';
+import { event, EventPlugin } from '@/event/mod';
+import Core, { DataFlow, Plugin } from '@biruni/core';
+import { MOCK_NAMESPACE, mockData, type MockData } from '@repo/mocks';
+import type { MockInstance } from 'vitest';
 
-describe('EventEmitter Plugin', () => {
-	it('Verify Signature', () => {
-		expect(event).toBeTypeOf('function')
-		expect(EventEmitterPlugin).toBeTypeOf('function')
-		const plugin = EventEmitterPlugin<MockData>()
-		expect(plugin).toHaveProperty('type')
-		expect(plugin).toHaveProperty('name')
-		expect(plugin).toHaveProperty('preprocess')
-		expect(plugin).toHaveProperty('postprocess')
-		expectTypeOf(plugin['type']).toEqualTypeOf<BiruniPlugin<MockData>['type']>()
-		expectTypeOf(plugin['name']).toBeString()
-	})
+describe('event', () => {
+	it('should be a <function>', () => {
+		expectTypeOf(event).toBeFunction();
+		expect(event).toBeTypeOf('function');
+	});
 
-	it('<preprocess> method should return same object of input', async () => {
-		const plugin = EventEmitterPlugin<MockData>();
-		const result = await plugin.preprocess(mockData);
-		expect(result).toBeTypeOf('object');
-		expect(result).toStrictEqual(expect.objectContaining(mockData))
-	})
+	it('should have no argument', () => {
+		expectTypeOf(event).parameters.toEqualTypeOf<[]>();
+	});
 
-	it('<postprocess> method should return same object of input', async () => {
-		const plugin = EventEmitterPlugin<MockData>();
-		const result = await plugin.postprocess(mockData);
-		expect(result).toBeTypeOf('object');
-		expect(result).toStrictEqual(expect.objectContaining(mockData))
-	})
+	it('should return an instance match to <Plugin> interface', () => {
+		expectTypeOf(event<MockData>).returns.toMatchTypeOf<Plugin<MockData>>();
+	});
 
-	it('<addListener> method should attach <change> event to listener callback function', () => {
-		const listener = vi.fn();
-		const plugin = EventEmitterPlugin<MockData>();
-		const add_listener_spy = vi.spyOn(EventEmitter.prototype, 'addListener');
-		plugin.addListener('change', listener);
-		expect(add_listener_spy).toBeCalledTimes(1);
-		expect(add_listener_spy).toBeCalledWith('change', listener);
-	})
+	it('should return an instance match to <EventPlugin> class', () => {
+		expectTypeOf(event<MockData>).returns.toEqualTypeOf<EventPlugin<MockData>>();
+		expect(event<MockData>()).toBeInstanceOf(EventPlugin<MockData>);
+	});
+});
 
-	it('<postprocess> method should invoke <listener> callback function', () => {
-		const listener = vi.fn();
-		const plugin = EventEmitterPlugin<MockData>();
-		plugin.addListener('change', listener);
-		plugin.postprocess(mockData);
-		expect(listener).toBeCalledTimes(1);
-		const diff_object = diff({}, mockData);
-		expect(listener).toBeCalledWith(expect.objectContaining(diff_object));
-	})
+describe('EventPlugin', () => {
+	let core: Core<MockData, typeof MOCK_NAMESPACE>;
+	let boundSpy: MockInstance;
+	let watchSpy: MockInstance;
+	let instance: Plugin<MockData>;
+	let store: any;
 
-	it('<removeListener> method should remove <change> and listener callback function', () => {
-		const listener = vi.fn();
-		const plugin = EventEmitterPlugin<MockData>();
+	beforeEach(() => {
+		core = new Core(MOCK_NAMESPACE);
+		instance = new EventPlugin<MockData>();
+		boundSpy = vi.spyOn(core, 'bound');
+		watchSpy = vi.spyOn(core, 'watch');
+		vi.clearAllMocks();
+		core.plug(instance);
+		store = core.init(vi.fn(() => mockData));
+	});
 
-		const add_listener_spy = vi.spyOn(EventEmitter.prototype, 'addListener');
-		plugin.addListener('change', listener);
-		expect(add_listener_spy).toBeCalledTimes(1);
-		expect(add_listener_spy).toBeCalledWith('change', listener);
+	it('should a class with <Plugin> interface', () => {
+		expectTypeOf(EventPlugin<MockData>).instance.toMatchTypeOf<Plugin<MockData>>();
+	});
 
-		const remove_listener_spy = vi.spyOn(EventEmitter.prototype, 'removeListener');
-		plugin.removeListener('change', listener);
-		expect(remove_listener_spy).toBeCalledTimes(1);
-		expect(remove_listener_spy).toBeCalledWith('change', listener);
-	})
-})
+	it('should invoke <Core.bound> around <4> times', () => {
+		expect(boundSpy).toHaveBeenCalledTimes(4);
+	});
+
+	it('should bound <addListener> to <Core> instance', () => {
+		expect(boundSpy).toHaveBeenCalledWith('addListener', expect.any(Function));
+	});
+
+	it('should bound <on> to <Core> instance', () => {
+		expect(boundSpy).toHaveBeenCalledWith('on', expect.any(Function));
+	});
+
+	it('should bound <removeListener> to <Core> instance', () => {
+		expect(boundSpy).toHaveBeenCalledWith('removeListener', expect.any(Function));
+	});
+
+	it('should bound <off> to <Core> instance', () => {
+		expect(boundSpy).toHaveBeenCalledWith('off', expect.any(Function));
+	});
+
+	it('should watch <DataFlow.OUTPUT> event', () => {
+		expect(watchSpy).toHaveBeenCalledWith(DataFlow.OUTPUT, expect.any(Function));
+	});
+
+	it('should watch <DataFlow.INPUT> event', () => {
+		expect(watchSpy).toHaveBeenCalledWith(DataFlow.INPUT, expect.any(Function));
+	});
+
+	// Ensure to have same reference
+	const onOutputMock = vi.fn().mockImplementation(function onOutputMock(data: MockData) {
+		expect(data).toMatchObject(mockData);
+	});
+
+	it('should trigger on <Core.process> invoke with <DataFlow.OUTPUT>', () => {
+		expect.hasAssertions();
+		store.on(DataFlow.OUTPUT, onOutputMock);
+		core.process(DataFlow.OUTPUT, () => mockData);
+	});
+
+	it('should remove <listener> correctly', () => {
+		store.off(DataFlow.OUTPUT, onOutputMock);
+		core.process(DataFlow.OUTPUT, () => mockData);
+		expect(onOutputMock).toBeCalledTimes(0);
+	});
+});
