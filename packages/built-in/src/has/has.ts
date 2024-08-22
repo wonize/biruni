5d1c58252771fa42ec244c2cb2dbcc356ae54e21
mikoloism <mikoloism@gmail.com>
2024-08-22 15:27:21 +0330

    refactor(built-in/has): support custom error object
    When <base> arguement is not object will throw error. And now that is customized.

--- packages/built-in/src/has/has.ts
+++ packages/built-in/src/has/has.ts
@@ -2,12 +2,21 @@ import type { DataObject } from '@biruni/core/helpers/type-utility';
 import { hasProperty } from 'dot-prop';
 import type { Path } from '../path/mod';
 
+class BaseTypeError extends TypeError {
+	public constructor(base: unknown) {
+		super();
+		const baseActualType = (base === null ? 'null' : typeof base).toUpperCase();
+		super.message = `The <${baseActualType}> is not shape of an <Object>!`;
+		super.name = BaseTypeError.name;
+	}
+}
+
 function hasOwnPropertyPath<
 	Data extends DataObject,
 	PathKey extends Path.From<Data> = Path.From<Data>,
 >(data: Data, path: PathKey): boolean {
 	if (typeof data !== 'object' || data === null) {
-		throw 'Error';
+		throw new BaseTypeError(data);
 	}
 
 	return hasProperty(data, path as unknown as string);
