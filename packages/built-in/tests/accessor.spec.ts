4a0c634ec347b7daabceb1dcc2a4a4d1cb4dd29d
mikoloism <mikoloism@gmail.com>
2024-08-22 16:48:00 +0330

    test(built-in/accessor): enhance and implement test cases as "mod" re-exporter

--- packages/built-in/tests/accessor.spec.ts
+++ packages/built-in/tests/accessor.spec.ts
@@ -1,37 +1,89 @@
 import * as mod from '@/accessor';
 
-it('should re-export <GetAccessor> and <getter>', () => {
-	expect(mod).toHaveProperty('GetAccessor');
-	expect(mod.GetAccessor).toBeTypeOf('object');
-	expect(mod.GetAccessor).not.toBeNull();
-	expect(mod.GetAccessor).not.toBeInstanceOf(Array);
-	expect(mod.getter).toBeTypeOf('function');
-	expect(mod).toHaveProperty('getter');
+describe('GetAccessor', () => {
+	it('should be defined in <accessor.ts> file', () => {
+		expect(mod).toHaveProperty('GetAccessor');
+		expect(mod.GetAccessor).toBeDefined();
+	});
+
+	it('should be an <Constructor>', () => {
+		expect(mod.GetAccessor).constructor();
+	});
 });
 
-it('should re-export <SetAccessor> and <setter>', () => {
-	expect(mod).toHaveProperty('SetAccessor');
-	expect(mod.SetAccessor).toBeTypeOf('object');
-	expect(mod.SetAccessor).not.toBeNull();
-	expect(mod.SetAccessor).not.toBeInstanceOf(Array);
-	expect(mod).toHaveProperty('setter');
-	expect(mod.setter).toBeTypeOf('function');
+describe('getter', () => {
+	it('should be defined in <accessor.ts> file', () => {
+		expect(mod).toHaveProperty('getter');
+		expect(mod.getter).toBeDefined();
+	});
+
+	it('should be a <function>', () => {
+		expect(mod.getter).toBeTypeOf('function');
+	});
 });
 
-it('should export <HasAccessor>, <hasOwn>', () => {
-	expect(mod).toHaveProperty('HasAccessor');
-	expect(mod.HasAccessor).toBeTypeOf('object');
-	expect(mod.HasAccessor).not.toBeNull();
-	expect(mod.SetAccessor).not.toBeInstanceOf(Array);
-	expect(mod).toHaveProperty('hasOwn');
-	expect(mod.hasOwn).toBeTypeOf('function');
+describe('SetAccessor', () => {
+	it('should be defined in <accessor.ts> file', () => {
+		expect(mod).toHaveProperty('SetAccessor');
+		expect(mod.SetAccessor).toBeDefined();
+	});
+
+	it('should be an <Constructor>', () => {
+		expect(mod.SetAccessor).constructor();
+	});
 });
 
-it('should export <PropertyAccessor> and <accessorizer>', () => {
-	expect(mod).toHaveProperty('PropertyAccessor');
-	expect(mod.PropertyAccessor).toBeTypeOf('object');
-	expect(mod.PropertyAccessor).not.toBeNull();
-	expect(mod.PropertyAccessor).not.toBeInstanceOf(Array);
-	expect(mod).toHaveProperty('accessorizer');
-	expect(mod.accessorizer).toBeTypeOf('function');
+describe('setter', () => {
+	it('should be defined in <accessor.ts> file', () => {
+		expect(mod).toHaveProperty('setter');
+		expect(mod.setter).toBeDefined();
+	});
+
+	it('should be a <function>', () => {
+		expect(mod.setter).toBeTypeOf('function');
+	});
+});
+
+describe('HasAccessor', () => {
+	it('should be defined in <accessor.ts> file', () => {
+		expect(mod).toHaveProperty('HasAccessor');
+		expect(mod.HasAccessor).toBeDefined();
+	});
+
+	it('should be an <Constructor>', () => {
+		expect(mod.HasAccessor).constructor();
+	});
+});
+
+describe('hasOwn', () => {
+	it('should be defined in <accessor.ts> file', () => {
+		expect(mod).toHaveProperty('hasOwn');
+		expect(mod.hasOwn).toBeDefined();
+	});
+
+	it('should be a <function>', () => {
+		expect(mod.hasOwn).toBeTypeOf('function');
+	});
+});
+
+describe('PropertyAccessor', () => {
+	it('should be defined in <accessor.ts> file', () => {
+		expect(mod).toHaveProperty('PropertyAccessor');
+		expect(mod.PropertyAccessor).toBeDefined();
+	});
+
+	it('should be an <Constructor>', () => {
+		expect(mod.PropertyAccessor).constructor();
+	});
+});
+
+describe('accessorizer', () => {
+	it('should be defined in <accessor.ts> file', () => {
+		expect(mod).toHaveProperty('accessorizer');
+		expect(mod.accessorizer).toBeDefined();
+	});
+
+	it('should be a <function>', () => {
+		expect(mod.accessorizer).toBeTypeOf('function');
+	});
 });
