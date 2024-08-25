44040a68067bdc25dfed8f1b5d4a411fae0fc00b
mikoloism <mikoloism@gmail.com>
2024-08-25 05:15:12 +0330

    test(core/helpers/diff): enhance and fix wrong module path

--- packages/core/tests/helpers/diff.spec.ts
+++ packages/core/tests/helpers/diff.spec.ts
@@ -1,54 +1,59 @@
-import { diff, type Diff } from '@/listener/diff';
+import { diff, type Diff } from '@/helpers/diff';
 import { mockData, type MockData } from '@repo/mocks';
 
-describe('listener/diff.ts', () => {
-	describe('Verify Signature', () => {
-		it('Verify Parameters and Returns', () => {
-			expect(diff<MockData, MockData>).toBeTypeOf('function');
-			expectTypeOf(diff<MockData, MockData>).toBeFunction();
-			expectTypeOf(diff<MockData, MockData>)
-				.parameter(0)
-				.toBeObject();
-			expectTypeOf(diff<MockData, MockData>)
-				.parameter(1)
-				.toBeObject();
-			expectTypeOf(diff<MockData, MockData>).returns.toBeObject();
-		});
-	});
+it('is a <function>', () => {
+	expectTypeOf(diff).toBeFunction();
+	expect(diff).toBeTypeOf('function');
+});
 
-	it('should detect chenged key and produce diff return object', () => {
-		const source = { ...mockData } satisfies MockData;
-		const target = { ...mockData, lang: 'FR' } satisfies MockData;
-		const expected = {
-			source: source,
-			target: target,
-			keys: ['lang'] as never,
-			diff: {
-				lang: {
-					source: source['lang'],
-					target: target['lang'],
-				},
+it('accept an <object> in first argument', () => {
+	expectTypeOf(diff<MockData, MockData>)
+		.parameter(0)
+		.toBeObject();
+});
+
+it('accept an <object> in second argument', () => {
+	expectTypeOf(diff<MockData, MockData>)
+		.parameter(1)
+		.toBeObject();
+});
+
+it('return an <object>', () => {
+	expectTypeOf(diff<MockData, MockData>).returns.toBeObject();
+});
+
+it('should detect chenged key and produce diff return object', () => {
+	const source = { ...mockData } satisfies MockData;
+	const target = { ...mockData, lang: 'FR' } satisfies MockData;
+	const expected = {
+		source: source,
+		target: target,
+		keys: ['lang'] as never,
+		diff: {
+			lang: {
+				source: source['lang'],
+				target: target['lang'],
 			},
-		} satisfies Diff<typeof source, typeof target, MockData>;
-		const result = diff(source, target);
-		expect(result).toStrictEqual(expected);
-	});
+		},
+	} satisfies Diff<typeof source, typeof target, MockData>;
+	const result = diff(source, target);
+	expect(result).toStrictEqual(expected);
+});
 
-	it('Verify <Diff> type shape', () => {
-		type DiffInput = Diff<
-			{ lang: 'EN'; theme: 'DARK' },
-			{ lang: 'ES'; theme: 'DARK' },
-			{ lang: 'EN' | 'ES'; theme: 'DARK' }
-		>;
+it('Verify <Diff> type shape', () => {
+	type DiffInput = Diff<
+		{ lang: 'EN'; theme: 'DARK' },
+		{ lang: 'ES'; theme: 'DARK' },
+		{ lang: 'EN' | 'ES'; theme: 'DARK' }
+	>;
 
-		interface DiffExpect {
-			source: { lang: 'EN'; theme: 'DARK' };
-			target: { lang: 'ES'; theme: 'DARK' };
-			diff: { lang: { source: 'EN'; target: 'ES' } };
-			keys: ['lang'];
-		}
+	interface DiffExpect {
+		source: { lang: 'EN'; theme: 'DARK' };
+		target: { lang: 'ES'; theme: 'DARK' };
+		diff: { lang: { source: 'EN'; target: 'ES' } };
+		keys: ['lang'];
+	}
 
-		// @ts-expect-error the `keys` property should be keyof diff property
-		expectTypeOf<DiffInput>().toEqualTypeOf<DiffExpect>();
-	});
+	// @ts-expect-error the `keys` property should be keyof diff property
+	expectTypeOf<DiffInput>().toEqualTypeOf<DiffExpect>();
 });
